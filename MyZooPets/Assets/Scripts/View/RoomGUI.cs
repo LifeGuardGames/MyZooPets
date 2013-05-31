87b2ee7ef674fb78edee907560603dfd0779fbd4
Kezhi Wu <kezhiwu@gmail.com>
2013-05-31 16:46:11 -0400

    move Bars to RoomGui

--- MyZooPets/Assets/Scripts/View/RoomGUI.cs
+++ MyZooPets/Assets/Scripts/View/RoomGUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class RoomGui : MonoBehaviour {
 	
 	// native dimensions
-    private const float NATIVE_WIDTH = 1280.0f;
+    private const float NATIVE_WIDTH = 1280.0f;    //screen size 
     private const float NATIVE_HEIGHT = 800.0f;
 	
 	private bool isMenuExpanded;
@@ -18,41 +18,118 @@ public class RoomGui : MonoBehaviour {
 	public Texture2D healthIcon;
 	public Texture2D moodIcon;
 	public Texture2D demopet;
+	public Texture2D progressBarFrame;
+	public Texture2D progressBarFill;
+	public Texture2D statBarVerFill;
+	public Texture2D statBarVerFrame;
+	
 	public GUIStyle starTextStyle;
+	public GUIStyle expreTextStyle;
+	public GUIStyle tierTextStyle;
+	
+	public float progress;
+	public float food;
+	public float mood;
+	public float health;
+		
+	private Vector2 tierBarloc;         
+	private Vector2 starBarloc;
+	private Vector2 healthBarloc;
+	private Vector2 moodBarloc;
+	private Vector2 foodBarloc;
+	private Vector2 starIconOffset;
+	private Vector2 healthIconOffset;
+	private Vector2 moodIconOffset;
+	private Vector2 foodIconOffset;
+	private Vector2 starTextOffset;
+	private Vector2 progressBarOffset;
+	private Vector2 progressTextOffset;
+	private Vector2 tierTextOffset;
+	private Vector2 healthBarOffset;
+	private Vector2 moodBarOffset;
+	private Vector2 foodbarOffset;
+	private string tierLevel;
+	private string tierProgressText;
+	private string starCount;
 	
 	void Start (){
+		progress = 50f;
+		food = 30f;
+		mood = 50f;
+		health = 80f;
 		isMenuExpanded = true;
 		menuRect = new LTRect(0, NATIVE_HEIGHT - 100, 475, 100);
+		tierBarloc = new Vector2(0,2);
+		starBarloc = new Vector2(540,2);
+		healthBarloc = new Vector2(0,80);
+		moodBarloc = new Vector2(0,180);
+		foodBarloc = new Vector2(0,280);
+		starIconOffset = new Vector2(10,4);
+		healthIconOffset = new Vector2(5,18);
+		moodIconOffset = new Vector2(5,18);
+		foodIconOffset = new Vector2(3,20);
+		starTextOffset = new Vector2(90,18);
+		progressBarOffset = new Vector2(150,11);
+		progressTextOffset = new Vector2(230,12);
+		tierTextOffset = new Vector2(25,12);
+		healthBarOffset = new Vector2(60,15);
+		moodBarOffset = new Vector2(60,15);
+		foodbarOffset = new Vector2(60,15);
 	}
 	
 	void Update (){
-	
+		tierLevel = "Tier 1";
+		tierProgressText = "5000/10000";
+		starCount = "500";
 	}
 	
 	void OnGUI(){
-		if (NATIVE_WIDTH != Screen.width || NATIVE_HEIGHT != Screen.height){
+		if (NATIVE_WIDTH != Screen.width || NATIVE_HEIGHT != Screen.height){     //porpotional scaling
             float horizRatio = Screen.width/NATIVE_WIDTH;
             float vertRatio = Screen.height/NATIVE_HEIGHT;
             GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(horizRatio, vertRatio, 1));
 		}
 	
-		GUI.DrawTexture(new Rect(0,0,1280,800), roomTexture);
-		GUI.DrawTexture(new Rect(0,2,530,75), tierBarTexture);
-		GUI.DrawTexture(new Rect(540,2,215,75), starBarTexture);
-		GUI.DrawTexture(new Rect(550,6,60,60), starTexture, ScaleMode.ScaleToFit);
-		GUI.DrawTexture(new Rect(0,80,100,100), statBarTexture);
-		GUI.DrawTexture(new Rect(0,180,100,100), statBarTexture);
-		GUI.DrawTexture(new Rect(0,280,100,100), statBarTexture);
-		GUI.DrawTexture(new Rect(5,100,54,53),healthIcon);
-		GUI.DrawTexture(new Rect(5,200,54,53),moodIcon);
-		GUI.DrawTexture(new Rect(3,300,61,53),foodIcon);
+		GUI.DrawTexture(new Rect(0,0,1280,800), roomTexture);  //temp room background 
+		GUI.DrawTexture(new Rect(330,300,500,500), demopet);   //temp demo pet
+		
+		
+		//Room GUI Positioning 
+		
+		//Progress Bar
+		GUI.DrawTexture(new Rect(tierBarloc.x,tierBarloc.y,530,75), tierBarTexture);
+		GUI.DrawTexture(new Rect(tierBarloc.x + progressBarOffset.x,tierBarloc.y+progressBarOffset.y,350,50),progressBarFrame);
+		GUI.DrawTexture(new Rect(tierBarloc.x + progressBarOffset.x,tierBarloc.y+progressBarOffset.y,350 * Mathf.Clamp01(progress/100),50),progressBarFill, ScaleMode.ScaleAndCrop, true, 150/13);
+		GUI.TextField(new Rect(tierBarloc.x + progressTextOffset.x,tierBarloc.y+progressTextOffset.y,200,40),tierProgressText,expreTextStyle);
+		GUI.TextField(new Rect(tierBarloc.x+tierTextOffset.x,tierBarloc.y+tierTextOffset.y,200,40),tierLevel,tierTextStyle);
+		
+		//Star Bar		
+		GUI.DrawTexture(new Rect(starBarloc.x,starBarloc.y,215,75), starBarTexture);
+		GUI.DrawTexture(new Rect(starBarloc.x + starIconOffset.x,starBarloc.y + starIconOffset.y,60,60), starTexture, ScaleMode.ScaleToFit);
+		GUI.TextField(new Rect(starBarloc.x+starTextOffset.x,starBarloc.y+starTextOffset.y,60,60),starCount,starTextStyle);            
+		
+		//Health Bar
+		GUI.DrawTexture(new Rect(healthBarloc.x,healthBarloc.y,100,100), statBarTexture);
+		GUI.DrawTexture(new Rect(healthBarloc.x + healthBarOffset.x,healthBarloc.y + healthBarOffset.y,25,70),statBarVerFrame);
+		GUI.DrawTexture(new Rect(healthBarloc.x + healthBarOffset.x,healthBarloc.y + healthBarOffset.y+(70-70*health/100),25, 70 * Mathf.Clamp01(health/100)),statBarVerFill, ScaleMode.ScaleAndCrop, true, 25/70);
+		GUI.DrawTexture(new Rect(healthBarloc.x + healthIconOffset.x,healthBarloc.y + healthIconOffset.y,60,60),healthIcon, ScaleMode.ScaleToFit, true, 0f);
+		
+		//Mood Bar	
+		GUI.DrawTexture(new Rect(moodBarloc.x,moodBarloc.y,100,100), statBarTexture);
+		GUI.DrawTexture(new Rect(moodBarloc.x + moodBarOffset.x,moodBarloc.y+moodBarOffset.y,25,70),statBarVerFrame);
+		GUI.DrawTexture(new Rect(moodBarloc.x + moodBarOffset.x,moodBarloc.y+moodBarOffset.y+(70-70*mood/100),25, 70 * Mathf.Clamp01(mood/100)),statBarVerFill, ScaleMode.ScaleAndCrop, true, 25/70);
+		GUI.DrawTexture(new Rect(moodBarloc.x + moodIconOffset.x,moodBarloc.y+moodIconOffset.y,60,60),moodIcon,ScaleMode.ScaleToFit, true, 0f);
+		
+		//Food Bar
+		GUI.DrawTexture(new Rect(foodBarloc.x,foodBarloc.y,100,100), statBarTexture);
+		GUI.DrawTexture(new Rect(foodBarloc.x + foodbarOffset.x,foodBarloc.y + foodbarOffset.y,25,70),statBarVerFrame);
+		GUI.DrawTexture(new Rect(foodBarloc.x + foodbarOffset.x,foodBarloc.y + foodbarOffset.y+(70-70*food/100),25, 70 * Mathf.Clamp01(food/100)),statBarVerFill, ScaleMode.ScaleAndCrop, true, 25/70);
+		GUI.DrawTexture(new Rect(foodBarloc.x + foodIconOffset.x,foodBarloc.y + foodIconOffset.y,60,60),foodIcon,ScaleMode.ScaleToFit, true, 0f);
 		
 		
 		
-		GUI.DrawTexture(new Rect(330,300,500,500), demopet);
 	   	
-		GUI.TextField(new Rect(630,20,60,60),"X 500",starTextStyle);
-		
+		//Extending Button Groups
 		GUILayout.BeginArea(menuRect.rect, "test");
 		GUILayout.BeginHorizontal("box");
 		GUILayout.Button("food", GUILayout.Height(90), GUILayout.Width(90));

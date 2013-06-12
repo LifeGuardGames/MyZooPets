e26cb2dfceaf18214a658e788f51db538f5e333e
Greg Wong <gwong1990@gmail.com>
2013-06-12 17:05:22 -0400

    Inhaler GUI

--- MyZooPets/Assets/Scripts/InhalerGame/InhalerGameGUI.cs
+++ MyZooPets/Assets/Scripts/InhalerGame/InhalerGameGUI.cs
@@ -2,48 +2,59 @@ using UnityEngine;
 using System.Collections;
 
 public class InhalerGameGUI : MonoBehaviour {
-	
-	public int numberOfNodes;
+
 	public float speed;
-	
+
 	public GUISkin defaultSkin;
 	public Texture2D circleGray;
 	public Texture2D circleRed;
-	
+
 	private int currentNode;
 	public float currentPercentage;
 	public float targetPercentage;
 	public float tParam;
-	
+
+	private int numberOfNodes;
     private Vector2 pos;
     private Vector2 size = new Vector2(1020, 40);
-	
+
 	private float segmentChunkPx;	// Pixels in between chunks
-	
+
 	private bool[] boolList;
-	
+
 	private bool isUpdating = false;
-	
+
 	void Awake(){
 	}
-	
+
 	void Start(){
 		currentNode = InhalerLogic.CurrentStep - 1; // Starting out with step 0 here
-		
+
+		SetNumOfNodes();
+
 		if(numberOfNodes < 2){
-			Debug.LogError("Number of nodes cannot be less than 2");	
+			Debug.LogError("Number of nodes cannot be less than 2");
 		}
 		pos = new Vector2(Screen.width/2 - size.x/2, 700);
 		currentNode = 0;
-		
+
 		segmentChunkPx = size.x / numberOfNodes;
-		
+
 		boolList = new bool[numberOfNodes];
 		for(int i = 0; i < numberOfNodes; i++){
-			boolList[i] = false;	
+			boolList[i] = false;
 		}
 	}
-	
+
+	void SetNumOfNodes(){
+		if (InhalerLogic.CurrentInhalerType == InhalerType.Advair){
+			numberOfNodes = 5;
+		}
+		if (InhalerLogic.CurrentInhalerType == InhalerType.Rescue){
+			numberOfNodes = 6;
+		}
+	}
+
 	void Update(){
 		if(currentNode != InhalerLogic.CurrentStep - 1){
 			UpdateBar();
@@ -62,24 +73,24 @@ public class InhalerGameGUI : MonoBehaviour {
 			tParam = 0;
 		}
 	}
-	
+
 	void OnGUI(){
 		GUI.skin = defaultSkin;
-		
+
 		//draw the background
 		GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
 		//GUI.Box(new Rect(0,0, size.x, size.y), emptyTex);
 		GUI.Box(new Rect(0,0, size.x, size.y), "");
-	
+
 	     //draw the filled-in part
 		GUI.BeginGroup(new Rect(0,0, size.x * currentPercentage, size.y));
-		
+
 		//GUI.Box(new Rect(0,0, size.x, size.y), fullTex);
 		GUI.Box(new Rect(0,0, size.x, size.y), "");
-		
+
 		GUI.EndGroup();
 	   	GUI.EndGroup();
-		
+
 		GUI.DrawTexture(new Rect(pos.x - circleGray.width / 2, 670, circleGray.width, circleGray.height), circleRed);
 		for(int i = 1; i <= numberOfNodes; i++){
 			if(boolList[i - 1]){
@@ -99,7 +110,7 @@ public class InhalerGameGUI : MonoBehaviour {
 			isUpdating = true;
 		}
 	}
-	
+
 	private void TickNodeOn(int i){
 		i--; // Account for array index offset
 		if(i < numberOfNodes && i >= 0){

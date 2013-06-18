002f262c28b8f928b76ea71fe8a18e1e9703e2c6
Greg Wong <gwong1990@gmail.com>
2013-06-18 16:59:25 -0400

    Pet gestures (not finalized yet)

--- MyZooPets/Assets/Scripts/View/ClickManager.cs
+++ MyZooPets/Assets/Scripts/View/ClickManager.cs
@@ -4,28 +4,28 @@ using System.Collections;
 /// <summary>
 /// Click manager.
 /// All the classes that need a click to enter a certain mode will be handled here (ie. diary, trophy, inhaler game)
-/// 
+///
 /// NOTE: When entering a mode, lock click and mode, when done transitioning, unlock click
 ///       When exiting a mode, unlock click and mode after finish transitioning
-/// 
+///
 /// </summary>
 
 public class ClickManager : MonoBehaviour {
 	private bool isMobilePlatform;
-	
+
 	// All the classes that need a click input go here
 	public GameObject diaryUIManagerObject;
 	private DiaryGUI diaryUIManager;
-	
+
 	public GameObject trophyGUIObject;
 	private TrophyGUI trophyGUI;
-	
+
 	public GameObject cameraMoveObject;
 	private CameraMove cameraMove;
-	
+
 	public static bool isClickLocked;	// Lock to prevent multiple clicking (diary + trophy modes at the same time)
 	public static bool isModeLocked;	// Lock to prevent clicking other objects when zoomed into a mode (clicking diary in trophy more)
-	
+
 	void Start(){
 		if(Application.platform == RuntimePlatform.Android ||
 			Application.platform == RuntimePlatform.IPhonePlayer){
@@ -37,7 +37,7 @@ public class ClickManager : MonoBehaviour {
 
 		isClickLocked = false;
 		isModeLocked = false;
-		
+
 		// Linking script references
 		diaryUIManager = diaryUIManagerObject.GetComponent<DiaryGUI>();
 		trophyGUI = trophyGUIObject.GetComponent<TrophyGUI>();
@@ -68,27 +68,39 @@ public class ClickManager : MonoBehaviour {
 							ClickLock();
 							ModeLock();
 						}
+						else if(hit.collider.name == "PetHead"){
+							// todo
+							print("Pet Head");
+							// ClickLock();
+							// ModeLock();
+						}
+						else if(hit.collider.name == "PetTummy"){
+							// todo
+							print("Pet Tummy");
+							// ClickLock();
+							// ModeLock();
+						}
 					}
 				}
 			}
 		}
 	}
-	
+
 	// Disable clicking when transitioning between modes
 	public static void ClickLock(){
-		isClickLocked = true;	
+		isClickLocked = true;
 	}
-	
+
 	// Enable clicking after the transitioning is done, usually called from LeanTween callback
 	public static void ReleaseClickLock(){
 		isClickLocked = false;
 	}
-	
+
 	// Disable clicking other objects inside a mode (ie, cant click shelf when in trophy mode)
 	public static void ModeLock(){
 		isModeLocked = true;
 	}
-	
+
 	// Enable clicking other objects after completed exiting a mode
 	public static void ReleaseModeLock(){
 		isModeLocked = false;

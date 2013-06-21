c6a1efbc40d151d21b112df4e29bd0b25acc6181
Kezhi Wu <kezhiwu@gmail.com>
2013-06-21 16:51:23 -0400

    add basic structure for Items

---
+++ MyZooPets/Assets/Scripts/Controller/ItemLogic.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemLogic : MonoBehaviour{
+
+	public static List<string> names = new List<string>();
+	public static List<Texture2D> textures = new List<Texture2D>();
+	public static List<int> costs = new List<int>();
+	
+	private Hashtable items = new Hashtable();
+	private Dictionary<object, Items.onUse> delegates = 
+		new Dictionary<object, Items.onUse>{
+		{names[0], takeApple},
+		{names[1], takeSandwich},
+		{names[2], takeInhaler},
+		{names[3], takeEmInhaler}
+	};
+	
+	public Items getItem(string name){
+		return (Items)items[name];
+	}
+	
+	public void OnCall(string name){
+		delegates[name]();
+	}
+	
+	
+	void Awake(){
+		if(names.Count == textures.Count && textures.Count == costs.Count){
+			for(int i = 0;i< names.Count;i++){
+				items.Add (names[i],new Items(names[i],costs[i],textures[i]));
+			}
+		}
+	}
+	
+	public static void takeApple(){
+		DataManager.AddHunger(10);
+	}
+	
+	public static void takeSandwich(){
+		DataManager.AddHunger(30);
+	}
+	
+	public static void takeInhaler(){
+		CalendarLogic.RecordGivingInhaler();
+	}
+	
+	public static void takeEmInhaler(){
+		
+	}
+}

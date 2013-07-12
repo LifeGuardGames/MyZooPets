ddee444bb5b1f7e12b6e8273d0bce934f60de645
Greg Wong <gwong1990@gmail.com>
2013-07-12 14:58:33 -0400

    removed deprecated code

--- MyZooPets/Assets/Scripts/View/Notifications/NotificationUIManager.cs
+++ MyZooPets/Assets/Scripts/View/Notifications/NotificationUIManager.cs
@@ -11,23 +11,16 @@ public class NotificationUIManager : MonoBehaviour {
 	// References
 	public GameObject cameraObject;
 	public GameObject NguiAnchor;
-	public GameObject popupTextureGreat;
 	public GameObject popupTextureGreatNGUI;
-	public GameObject popupTextureNiceTry;
 	public GameObject popupTextureNiceTryNGUI;
-	public GameObject popupTextureUseTheInhaler;
 	public GameObject popupTextureUseTheInhalerNGUI;
-	public GameObject popupTexturePracticeInhaler;
 	public GameObject popupTexturePracticeInhalerNGUI;
-	public GameObject popupTextureDiagnoseSymptoms;
 	public GameObject popupTextureDiagnoseSymptomsNGUI;
 	public GameObject popupAward;
 
-	public GameObject popupNotification;
 	public GameObject popupNotificationOneButton;
 	public GameObject popupNotificationTwoButtons;
 	public GameObject levelUpMessage;
-	public GameObject gameOverRewardMessage;
 	public GameObject gameOverRewardMessageOneButton;
 	public GameObject gameOverRewardMessageTwoButtons;
 	public bool flipped;
@@ -44,75 +37,13 @@ public class NotificationUIManager : MonoBehaviour {
 				cameraObject.transform.position.y - 1f, cameraObject.transform.position.z - 4f);
 		}
 	}
-	//========================Deprecated===================================
-
-	/*
-		Desc: creates a popup with a texture and stats that have been modified
-		Params: notificationType, deltaPoints, deltaStars, deltaHealth, deltaMood, deltaHunger
-	*/
-	public void PopupTexture(string notificationType, int deltaPoints, int deltaStars,
-		int deltaHealth, int deltaMood, int deltaHunger){
-		switch(notificationType){
-			case "award":
-				GameObject go = Instantiate(popupTextureGreat, gameObject.transform.position, Quaternion.identity) as GameObject;
-				Destroy(go, 3.0f);
-
-				GameObject go2 = Instantiate(popupAward, gameObject.transform.position, Quaternion.identity) as GameObject;
-				PopupAward script = go2.GetComponent<PopupAward>();		// Make sure that the award object has a script to populate
-				if(script != null){
-					script.Populate(deltaPoints, deltaStars, deltaHealth, deltaMood, deltaHunger);
-				}else{
-					Debug.LogError("Script attachment missing");
-				}
-				Destroy(go2, 3.0f);
-			break;
-
-			case "nice try":
-				GameObject go3 = Instantiate(popupTextureNiceTry, gameObject.transform.position, Quaternion.identity) as GameObject;
-				Destroy(go3, 3.0f);
-			break;
-		}
-
-	}
 	//==========================================================
 	/*
 		Desc: creates a popup with only a texture
 		Params: notificationType
 	*/
 	public void PopupTexture(string notificationType){
-		// GameObject prefab = null;
-		// switch(notificationType){
-		// 	case "great":
-		// 		prefab = popupTextureGreat;
-		// 	break;
 
-		// 	case "practice intro":
-		// 		prefab = popupTexturePracticeInhaler;
-		// 	break;
-
-		// 	case "intro":
-		// 		prefab = popupTextureUseTheInhaler;
-		// 	break;
-
-		// 	case "nice try":
-		// 		prefab = popupTextureNiceTry;
-		// 	break;
-
-		// 	case "diagnose":
-		// 		prefab = popupTextureDiagnoseSymptoms;
-		// 	break;
-		// }
-		// if(prefab == popupTexturePracticeInhaler){
-		// 	GameObject go = Instantiate(prefab, gameObject.transform.position, Quaternion.identity) as GameObject;
-		// 	Destroy(go, 3.0f);
-
-		// 	// show regular intro after announcing that it is a practice game
-		// 	Invoke("ShowIntro", 3.0f);
-		// }
-		// else if(prefab != null){
-		// 	GameObject go = Instantiate(prefab, gameObject.transform.position, Quaternion.identity) as GameObject;
-		// 	Destroy(go, 3.0f);
-		// }
 		GameObject prefab = null;
 		switch(notificationType){
 			case "great":
@@ -171,12 +102,7 @@ public class NotificationUIManager : MonoBehaviour {
 	*/
 	public void PopupNotificationTwoButtons(string message, PopupNotificationNGUI.Callback yesCallBack,
 		PopupNotificationNGUI.Callback noCallBack){
-		// GameObject go = Instantiate(popupNotification, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// PopupNotification script = go.GetComponent<PopupNotification>();
-		// if(script != null){
-		// 	script.Init(message, yesCallBack, noCallBack);
-		// }
+
 		PopupNotificationNGUI twoButtonMessage = CreatePopupNotificationNGUI(popupNotificationTwoButtons);
 		twoButtonMessage.Message = message;
 		twoButtonMessage.Button1Callback = yesCallBack;
@@ -188,14 +114,7 @@ public class NotificationUIManager : MonoBehaviour {
 
 	public void PopupNotificationTwoButtons(string message, PopupNotificationNGUI.Callback yesCallBack,
 		PopupNotificationNGUI.Callback noCallBack, string button1, string button2){
-		// GameObject go = Instantiate(popupNotification, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// PopupNotification script = go.GetComponent<PopupNotification>();
-		// if(script != null){
-		// 	script.Button1String = button1;
-		// 	script.Button2String = button2;
-		// 	script.Init(message, yesCallBack, noCallBack);
-		// }
+
 		PopupNotificationNGUI twoButtonMessage = CreatePopupNotificationNGUI(popupNotificationTwoButtons);
 		twoButtonMessage.Message = message;
 		twoButtonMessage.Button1Callback = yesCallBack;
@@ -210,12 +129,6 @@ public class NotificationUIManager : MonoBehaviour {
 		Params: notificationType, call back for button
 	*/
 	public void PopupNotificationOneButton(string message, PopupNotificationNGUI.Callback okCallBack){
-		// GameObject go = Instantiate(popupNotification, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// PopupNotification script = go.GetComponent<PopupNotification>();
-		// if(script != null){
-		// 	script.Init(message, okCallBack);
-		// }
 
 		PopupNotificationNGUI oneButtonMessage = CreatePopupNotificationNGUI(popupNotificationOneButton);
 		oneButtonMessage.Message = message;
@@ -224,13 +137,6 @@ public class NotificationUIManager : MonoBehaviour {
 		oneButtonMessage.Display();
 	}
 	public void PopupNotificationOneButton(string message, PopupNotificationNGUI.Callback okCallBack, string button){
-		// GameObject go = Instantiate(popupNotification, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// PopupNotification script = go.GetComponent<PopupNotification>();
-		// if(script != null){
-		// 	script.Button1String = button;
-		// 	script.Init(message, okCallBack);
-		// }
 
 		PopupNotificationNGUI oneButtonMessage = CreatePopupNotificationNGUI(popupNotificationOneButton);
 		oneButtonMessage.Message = message;
@@ -269,13 +175,6 @@ public class NotificationUIManager : MonoBehaviour {
 		PopupNotificationNGUI.Callback yesButtonCallBack,
 		PopupNotificationNGUI.Callback noButtonCallBack){
 
-		// GameObject go = Instantiate(gameOverRewardMessage, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// GameOverRewardMessage script = go.GetComponent<GameOverRewardMessage>();
-		// if(script != null){
-		// 	script.Init(deltaStars, deltaPoints, yesButtonCallBack, noButtonCallBack);
-		// }
-
 		GameOverRewardMessageNGUI twoButtonMessage = CreatePopupNotificationNGUI(gameOverRewardMessageTwoButtons) as GameOverRewardMessageNGUI;
 		twoButtonMessage.SetRewardMessage(deltaStars, deltaPoints);
 		twoButtonMessage.Button1Callback = yesButtonCallBack;
@@ -291,13 +190,6 @@ public class NotificationUIManager : MonoBehaviour {
 	*/
 	public void GameOverRewardMessage(int deltaStars, int deltaPoints,
 		PopupNotificationNGUI.Callback yesButtonCallBack){
-
-		// GameObject go = Instantiate(gameOverRewardMessage, gameObject.transform.position,
-		// 	Quaternion.identity) as GameObject;
-		// GameOverRewardMessage script = go.GetComponent<GameOverRewardMessage>();
-		// if(script != null){
-		// 	script.Init(deltaStars, deltaPoints, yesButtonCallBack);
-		// }
 
 		GameOverRewardMessageNGUI oneButtonMessage = CreatePopupNotificationNGUI(gameOverRewardMessageOneButton) as GameOverRewardMessageNGUI;
 		oneButtonMessage.SetRewardMessage(deltaStars, deltaPoints);

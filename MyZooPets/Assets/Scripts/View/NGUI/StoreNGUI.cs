967d71c6efc01900b04205d3fa546cd8918e2aab
Kezhi Wu <kezhiwu@gmail.com>
2013-07-15 18:40:19 -0400

    storeGUI without proper texture done

--- MyZooPets/Assets/Scripts/View/NGUI/StoreNGUI.cs
+++ MyZooPets/Assets/Scripts/View/NGUI/StoreNGUI.cs
@@ -38,16 +38,16 @@ public class StoreNGUI : MonoBehaviour {
 		//When clicked, first destory every item
 		GameObject grid = GameObject.Find("Grid");
 		foreach(Transform child in grid.transform){
-//			Destroy(child.gameObject);
+			Destroy(child.gameObject);
 		}
 		
 		if(page == null || page.name == "Food"){
 			uisprite.atlas = BackGroundRed;
 			for(int i = 0;i<itemlogic.foodlist.Count;i++){
 				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
-				item.transform.localScale = new Vector3(1.2f,1.3f,0);
+				item.transform.localScale = new Vector3(1.2f,1.3f,1);
 				item.name = itemlogic.foodlist[i].ToString();
-//				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelBlue";
+				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelBlue";
 				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.foodlist[i]].name;
 				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.foodlist[i]].name;
 				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
@@ -59,9 +59,9 @@ public class StoreNGUI : MonoBehaviour {
 			uisprite.atlas = BackGroundGreen;
 			for(int i = 0;i<itemlogic.itemlist.Count;i++){
 				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
-				item.transform.localScale = new Vector3(1.2f,1.3f,0);
+				item.transform.localScale = new Vector3(1.2f,1.3f,1);
 				item.name = itemlogic.itemlist[i].ToString();
-//				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelRed";
+				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelRed";
 				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.itemlist[i]].name;
 				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.itemlist[i]].name;
 				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
@@ -73,9 +73,9 @@ public class StoreNGUI : MonoBehaviour {
 			uisprite.atlas = BackGroundPurple;
 			for(int i = 0;i<itemlogic.decolist.Count;i++){
 				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
-				item.transform.localScale = new Vector3(1.2f,1.3f,0);
+				item.transform.localScale = new Vector3(1.2f,1.3f,1);
 				item.name = itemlogic.decolist[i].ToString();
-//				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelPurple";
+				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelPurple";
 				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.decolist[i]].name;
 				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.decolist[i]].name;
 				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
@@ -87,9 +87,9 @@ public class StoreNGUI : MonoBehaviour {
 			uisprite.atlas = BackGroundGreen;
 			for(int i = 0;i<itemlogic.inhalerlist.Count;i++){
 				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
-				item.transform.localScale = new Vector3(1.2f,1.3f,0);
+				item.transform.localScale = new Vector3(1.2f,1.3f,1);
 				item.name = itemlogic.inhalerlist[i].ToString();
-//				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelYellow";
+				item.transform.FindChild("ItemBackground").GetComponent<UISprite>().spriteName = "panelYellow";
 				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.inhalerlist[i]].name;
 				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.inhalerlist[i]].name;
 				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");

134a57a32bd9cedcbb4f0efb6f2fdc5704bd5b33
Greg Wong <gwong1990@gmail.com>
2013-07-15 19:52:46 -0400

    Progress Bar - Step Markers now automatically update based on the corresponding UISlider values

--- MyZooPets/Assets/Scripts/View/NGUI/ProgressBarAnimation.cs
+++ MyZooPets/Assets/Scripts/View/NGUI/ProgressBarAnimation.cs
@@ -7,12 +7,19 @@ public class ProgressBarAnimation : MonoBehaviour {
     public UISlider slider; // for setup
     public GameObject progressStep;
     private List<GameObject> markers;
+    private int stepCompleted;
+    private bool initCalled = false;
 
+    // Call this once at the beginning, after having instantiated the Progress Bar.
 	public void Init () {
+        initCalled = true;
+
         markers = new List<GameObject>();
         SetUpProgressSteps();
+        UpdateMarkerColors();
 	}
 
+    // Set up the markers, based on how the slider is set up.
     void SetUpProgressSteps(){
         Transform foreground = slider.transform.Find("Foreground");
         float width = foreground.transform.localScale.x;
@@ -33,7 +40,24 @@ public class ProgressBarAnimation : MonoBehaviour {
     }
 
     public void OnSliderChange(){
-        int step = (int) (slider.sliderValue * (slider.numberOfSteps - 1)); // same as slider.sliderValue / (1 / (slider.numberOfSteps - 1))
-        Debug.Log("Step " + step + " completed.");
+        if (!initCalled) return;
+
+        // Update the number of steps completed.
+        stepCompleted = (int) (slider.sliderValue * (slider.numberOfSteps - 1)); // same as slider.sliderValue / (1 / (slider.numberOfSteps - 1))
+        Debug.Log("Step " + stepCompleted + " completed.");
+
+        UpdateMarkerColors();
+    }
+
+    // Loop through all step markers, and set their colors accordingly.
+    void UpdateMarkerColors(){
+        for (int i = 0; i < markers.Count; i++){
+            if (i <= stepCompleted){
+                markers[i].transform.Find("Sprite").GetComponent<UISprite>().spriteName="circleRed";
+            }
+            else {
+                markers[i].transform.Find("Sprite").GetComponent<UISprite>().spriteName="circleGray";
+            }
+        }
     }
 }

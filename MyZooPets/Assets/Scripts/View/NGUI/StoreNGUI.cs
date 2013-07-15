f33a82c46afda992aaf6401ef497139471babca8
Kezhi Wu <kezhiwu@gmail.com>
2013-07-15 15:13:55 -0400

    new store NGUI

--- MyZooPets/Assets/Scripts/View/NGUI/StoreNGUI.cs
+++ MyZooPets/Assets/Scripts/View/NGUI/StoreNGUI.cs
@@ -7,16 +7,15 @@ public class StoreNGUI : MonoBehaviour {
 	
 	public GameObject ItemPrefab;
 	
+	private ItemLogic itemlogic;
+	private bool changePage;
+	private int page;
+	
 	// Use this for initialization
 	void Start () {
-		GameObject item = (GameObject)Instantiate(ItemPrefab);
-		item.SetParent(GameObject.Find ("Grid"));
-		item.name = "haha";
-		
-		item = (GameObject)Instantiate(ItemPrefab);
-		item.name = "jj";
-		item.SetParent(GameObject.Find ("Grid"));
-		GameObject.Find("Grid").GetComponent<UIGrid>().Reposition();
+		//todo Change to GameManager later
+		itemlogic = GameObject.Find("Grid").GetComponent<ItemLogic>();
+		createItems(null);
 	}
 	
 	// Update is called once per frame
@@ -24,13 +23,68 @@ public class StoreNGUI : MonoBehaviour {
 	
 	}
 	
-	public void lala(GameObject a){
-		print (a);
-//		print (a);
-		print ("lalalal");
+	void OnBuyButton(GameObject button){
+		itemlogic.OnCall(Convert.ToInt32(button.transform.parent.name));
 	}
 	
-	void haha(){
+	void createItems(GameObject page){
+		
+		//When clicked, first destory every item
+		GameObject grid = GameObject.Find("Grid");
+		foreach(Transform child in grid.transform){
+			Destroy(child.gameObject);
+		}
+		
+		if(page == null || page.name == "Food"){
+			for(int i = 0;i<itemlogic.foodlist.Count;i++){
+				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
+				item.name = itemlogic.foodlist[i].ToString();
+				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.foodlist[i]].name;
+				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.foodlist[i]].name;
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().functionName = "OnBuyButton";
+				//Todo add Background and Description
+			}
+		}
+		else if(page.name == "Item"){
+			for(int i = 0;i<itemlogic.itemlist.Count;i++){
+				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
+				item.name = itemlogic.itemlist[i].ToString();
+				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.itemlist[i]].name;
+				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.itemlist[i]].name;
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().functionName = "OnBuyButton";
+				//Todo add Background and Description
+			}
+		}
+		else if(page.name == "Decoration"){
+			for(int i = 0;i<itemlogic.decolist.Count;i++){
+				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
+				item.name = itemlogic.decolist[i].ToString();
+				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.decolist[i]].name;
+				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.decolist[i]].name;
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().functionName = "OnBuyButton";
+				//Todo add Background and Description
+			}
+		}
+		else if(page.name == "Inhaler"){
+			for(int i = 0;i<itemlogic.inhalerlist.Count;i++){
+				GameObject item = NGUITools.AddChild(GameObject.Find("Grid"),ItemPrefab);
+				item.name = itemlogic.inhalerlist[i].ToString();
+				item.transform.FindChild("ItemName").GetComponent<UILabel>().text = itemlogic.items[itemlogic.inhalerlist[i]].name;
+				item.transform.FindChild("ItemTexture").GetComponent<UISprite>().spriteName = itemlogic.items[itemlogic.inhalerlist[i]].name;
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().target = GameObject.Find("StoreGUI");
+				item.transform.FindChild("BuyButton").GetComponent<UIButtonMessage>().functionName = "OnBuyButton";
+				//Todo add Background and Description
+			}
+		}
+		grid.GetComponent<UIGrid>().Reposition();
+		Invoke("reposition",0.00000001f);
+	}
+	
+	void reposition(){
+		GameObject.Find("Grid").GetComponent<UIGrid>().Reposition();
 		
 	}
 }

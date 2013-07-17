780fc4216d4cd01027377e8fa1ced983f18aa172
Kezhi Wu <kezhiwu@gmail.com>
2013-07-17 14:23:20 -0400

    added new items

--- MyZooPets/Assets/Scripts/Controller/ItemLogic.cs
+++ MyZooPets/Assets/Scripts/Controller/ItemLogic.cs
@@ -49,17 +49,23 @@ public class ItemLogic : MonoBehaviour{
 	//This methos has to expand when more items added
 	private void LoadMethods(){
 		methods.Add(()=>TakeApple());
+		methods.Add(()=>TakeGreenApple());
 		methods.Add(()=>TakeSandwich());
-		methods.Add(()=>UseCarpet(items[2].texture));
-		methods.Add(()=>UseCarpet(items[3].texture));
-		methods.Add(()=>UseCarpet(items[4].texture));
+		methods.Add(()=>TakeBread());
+		methods.Add(()=>TakeDoughnut());
+		methods.Add(()=>TakeDoughnutBrown());
+		methods.Add(()=>TakeMilk());
+		
 	}
 	
 	private void AddDescription(){
-		items[0].description = "Health + 50";
-		items[1].description = "Health + 100";
-		items[2].description = "Cute Teddy";
-		items[3].description = "Brand New!";
+		items[0].description = "Mood + 10";
+		items[1].description = "Mood + 5";
+		items[2].description = "Mood + 30";
+		items[3].description = "Mood + 15";
+		items[4].description = "Mood + 20";
+		items[5].description = "Mood + 25";
+		items[6].description = "Mood + 40";
 	}
 	
 	void Awake(){
@@ -73,16 +79,31 @@ public class ItemLogic : MonoBehaviour{
 		AddDescription();
 	}
 
-	//Functions for Each item. 
+	//Functions for Each item.
 	public static void TakeApple(){
 		DataManager.AddMood(10);
 	}
-	
+	public static void TakeGreenApple(){
+		DataManager.AddMood(5);
+	}
 	public static void TakeSandwich(){
 		DataManager.AddMood(30);
 	}
-	
-	public static void UseCarpet(Texture texture){
-		GameObject.Find("Floor Rectangular").renderer.material.SetTexture("_MainTex",texture);
+	public static void TakeBread(){
+		DataManager.AddMood(15);
 	}
+	public static void TakeDoughnut(){
+		DataManager.AddMood(20);
+	}
+	public static void TakeDoughnutBrown(){
+		DataManager.AddMood(25);
+	}
+	public static void TakeMilk(){
+		DataManager.AddMood(40);
+	}
+	
+	//Template for Room Decoration
+//	public static void UseCarpet(Texture texture){
+//		GameObject.Find("Floor Rectangular").renderer.material.SetTexture("_MainTex",texture);
+//	}
 }

58be610c74b421335da10b8cd7872af377df14a2
Greg Wong <gwong1990@gmail.com>
2013-07-22 14:17:40 -0400

    renamed file

---
+++ MyZooPets/Assets/Scripts/View/NGUI/Notifications/PopupNotificationWithImageNGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupNotificationWithImageNGUI : PopupNotificationNGUI {
+
+    public UISprite imageSprite;
+
+    public void SetSprite(string name){
+        imageSprite.spriteName = name;
+    }
+
+}

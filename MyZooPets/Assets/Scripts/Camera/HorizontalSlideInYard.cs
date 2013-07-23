61cd4340c4438d17a63c89e5eb736f450eec80e5
Greg Wong <gwong1990@gmail.com>
2013-07-23 14:58:28 -0400

    limiting navigation in the yard

--- MyZooPets/Assets/Scripts/Camera/HorizontalSlideInYard.cs
+++ MyZooPets/Assets/Scripts/Camera/HorizontalSlideInYard.cs
@@ -7,39 +7,46 @@ public class HorizontalSlideInYard : UserNavigation {
     public float slideIncrement = 40f;
     Hashtable optional = new Hashtable();
     bool lockSlide = false;
+
+    float minLeft;
+    float maxRight;
+
     // Use this for initialization
     protected override void Start () {
         base.Start();
         currentXPos = transform.position.x;
         optional.Add("onComplete", "FinishedSlide");
+
+        // Init limits to yard navigation
+        minLeft = -slideIncrement;
+        maxRight = slideIncrement;
     }
 
     public override void ToTheRight(){
-        if (!lockSlide){
-            // if (ClickManager.CanRespondToTap()){
+        if (IsRightArrowEnabled()){
+            if (ClickManager.CanRespondToTap()){
                 lockSlide = true;
                 currentXPos += slideIncrement;
                 LeanTween.moveX(gameObject, currentXPos, 1.0f, optional);
-            // }
+            }
         }
     }
 
     public override void ToTheLeft(){
-        if (!lockSlide){
-            // if (ClickManager.CanRespondToTap()){
+        if (IsLeftArrowEnabled()){
+            if (ClickManager.CanRespondToTap()){
                 lockSlide = true;
                 currentXPos -= slideIncrement;
                 LeanTween.moveX(gameObject, currentXPos, 1.0f, optional);
-            // }
+            }
         }
     }
-
     protected override bool IsRightArrowEnabled(){
-        return true;
+        return (!lockSlide && currentXPos < maxRight);
     }
 
     protected override bool IsLeftArrowEnabled(){
-        return true;
+        return (!lockSlide && currentXPos > minLeft);
     }
 
     void FinishedSlide(){

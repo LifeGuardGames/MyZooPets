44495dfa0f410e0a67a2a9f426dca5e8f9ef79e7
Greg Wong <gwong1990@gmail.com>
2013-07-24 15:38:27 -0400

    removed unused code

--- MyZooPets/Assets/Scripts/View/NGUI/InhalerGameNGUI.cs
+++ MyZooPets/Assets/Scripts/View/NGUI/InhalerGameNGUI.cs
@@ -46,7 +46,6 @@ public class InhalerGameNGUI : MonoBehaviour {
                 delegate (){
                     inhalerGameManager.ResetInhalerGame();
                     RestartProgressBar();
-                    // ShowButtons();
                 },
                 QuitInhalerGame
             );

9caab395daedfc410f3376b910e568d7743487ce
Jason Cheng <yannru.cheng@gmail.com>
2013-07-25 21:15:47 -0400

    add speech capability to pet

---
+++ MyZooPets/Assets/Scripts/PetInteraction/Speech.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speech : MonoBehaviour {
+    public GameObject textGameObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Talk(string words){
+        textGameObject.GetComponent<TextMesh>().text = words;
+    }
+
+
+}

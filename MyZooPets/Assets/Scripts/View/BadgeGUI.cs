7f5229904e4c3c96b6455632ab7786838f1e8958
Wenshiang Chung <aliken9@gmail.com>
2013-08-01 15:04:17 -0400

    refactoring badgeGUI

--- MyZooPets/Assets/Scripts/View/BadgeGUI.cs
+++ MyZooPets/Assets/Scripts/View/BadgeGUI.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System;
 
 public class BadgeGUI : MonoBehaviour {
-
 	//======================Event=============================
     public static event EventHandler<EventArgs> OnBadgeBoardClosed;
     //=======================================================
@@ -12,30 +11,36 @@ public class BadgeGUI : MonoBehaviour {
 	public GUISkin defaultSkin;
 	public Texture2D backButton;
 	public GUIStyle blankButtonStyle;
+	private bool isActive = false;
 
 	public GameObject badgeBoard;
 	public GameObject descriptionObject;
-	public GameObject badgeGUISpawnBase;	// Parent to clone badges and zoom out
-
-	private bool isActive = false;
-
+	public GameObject badgeGUISpawnBase;	// Parent to clone badges in (anchor-center) when zoomed in
 	private GameObject bgPanel;
 
-	public List<GameObject> LevelList = new List<GameObject>(); //list of badge gameobjects
-																//index of this list correlates to the index
-																//from BadgeLogic.Instance.LevelBadges
+	// List of badge gameobjects
+	public List<GameObject> LevelList = new List<GameObject>();	// Index of this list correlates to the index from BadgeLogic.Instance.LevelBadges
+
+	public UIAtlas badgeCommonAtlas;		// Holds ALL the low-res badges and common objects
+	public UIAtlas badgeExtraAtlas;			// Holds tier (gold/silver/bronze) medals for zoomed display
+
+	// High Definition badges go here for closeup
+	// NOTE: 512x512 px, zero padding to fit extra row
 	public UIAtlas badgeLevelAtlas1;
 	public UIAtlas badgeLevelAtlas2;
 	public UIAtlas badgeLevelAtlas3;
-	public UIAtlas badgeCommonAtlas;	// Holds all the low-res badges and common objects
-	public UIAtlas badgeExtraAtlas;		// Holds the tier badges for zoomed display
 
-	//TODO-s refactor this dam script!!!!
-	// Use this for initialization
-	void Start (){
+	// Populate badges on plaque here
+	void Start(){
+
+		// Level Badges
 		foreach(Badge badge in BadgeLogic.Instance.LevelBadges){
 			int levelNumber = badge.ID;
+
+			// Populate metadata script in object
 			BadgeMetadata meta = LevelList[levelNumber].AddComponent<BadgeMetadata>();
+			meta.title = badge.name;
+			meta.description = badge.description;
 
 			if(badge.ID <= 7){
 				meta.atlasName = "BadgeLevelAtlas1";
@@ -47,11 +52,7 @@ public class BadgeGUI : MonoBehaviour {
 				meta.atlasName = "BadgeLevelAtlas3";
 			}
 
-			meta.title = badge.name;
-			meta.description = badge.description;
-
-			//Debug.Log(meta.title + " " + meta.description);
-
+			// Decide which sprite to use
 			if(badge.IsAwarded){
 				LevelList[levelNumber].transform.Find("badgeSprite").GetComponent<UISprite>().spriteName = "badgeLevel" + levelNumber;
 
@@ -59,14 +60,11 @@ public class BadgeGUI : MonoBehaviour {
 				if(badge.Tier != BadgeTier.Null){
 					UISprite tier = NGUITools.AddSprite(LevelList[levelNumber], badgeCommonAtlas, "badgeAddon" + badge.Tier.ToString());
 					tier.gameObject.name = "tier";
-
-					//TO-DO s, scale incorrect
 					tier.transform.localScale = new Vector3(39f, 50f, 1f);
 					tier.transform.localPosition = new Vector3(40f, -40f, 0);
 				}
 			}
-			else{
-				// Activate the dark version of the badge
+			else{	// Dark version of the badge
 				LevelList[levelNumber].transform.Find("badgeSprite").GetComponent<UISprite>().spriteName = "badgeLevel" + levelNumber + "Dark";
 			}
 		}
@@ -87,24 +85,14 @@ public class BadgeGUI : MonoBehaviour {
 		}
 	}
 
-	public void disableBackButton(){
-		isActive = false;
-	}
-
-	// // Parses the level of the badge
-	// private int parseLevelsBadge(string badgeName){
-	// 	return int.Parse(badgeName.Substring(badgeName.IndexOf(" ") + 1));
-	// }
-
 	// When a badge is clicked. Zoom in on the badge and display detail information
 	public void BadgeClicked(GameObject go){
-		disableBackButton();
+		DisableBackButton();
 
 		BadgeMetadata meta = go.GetComponent<BadgeMetadata>();
 
 		if(meta != null){
-
-			// Fine the appropriate atlas // TODO-s load resource dynamically?
+			// Find the appropriate atlas TODO-s load resource dynamically?
 			UIAtlas activeAtlas = null;
 			if(meta.atlasName == badgeLevelAtlas1.name){
 				activeAtlas = badgeLevelAtlas1;
@@ -120,19 +108,18 @@ public class BadgeGUI : MonoBehaviour {
 
 			UILabel titleLabel = titleGO.GetComponent<UILabel>();
 			titleLabel.text = meta.title;
-			Debug.Log(titleLabel.text);
 
 			GameObject descriptionGO = GameObject.Find("Label_Description") as GameObject;
 			UILabel descriptionLabel = descriptionGO.GetComponent<UILabel>();
 			descriptionLabel.text = meta.description;
 
 			// Spawn BG with collider
-			// Parent object needs to be 0, 0px;
+			// NOTE: Parent object needs to be 0, 0px, lower left corner;
 			UISprite bgSprite = NGUITools.AddSprite(descriptionObject, badgeCommonAtlas, "box30");
 			bgSprite.type = UISprite.Type.Sliced;
 			bgSprite.color = new Color(0f, 0f, 0f, 0.6f);
 			bgSprite.depth = 50;
-			bgPanel = bgSprite.gameObject;
+			bgPanel = bgSprite.gameObject;	// Get reference to delete later
 			bgPanel.transform.localScale = new Vector3(3000f, 3000f, 1);
 			bgPanel.transform.localPosition = new Vector3(0f, 0f ,0f);
 			BoxCollider collider = bgPanel.AddComponent<BoxCollider>();
@@ -147,17 +134,13 @@ public class BadgeGUI : MonoBehaviour {
 			badgeSprite.transform.localPosition = new Vector3(position.x, position.y, -1f);
 			badgeSprite.transform.localScale = new Vector3(100f, 100f, 0f);
 			badgeSprite.depth = 103;
-	
+
 			LeanTween.moveLocal(badgeSprite.gameObject, new Vector3(400, 400f, -1f), 0.4f);
 			LeanTween.scale(badgeSprite.gameObject, new Vector3(512f, 512, 0f), 0.4f);
-	
-	
-	
-			GameObject tierObject = GameObject.Find(go.name + "/tier");
 
+			GameObject tierObject = GameObject.Find(go.name + "/tier");
 			if(tierObject != null){
 				UISprite originalTier = tierObject.GetComponent<UISprite>();
-				// TODO refactor into function, used twice
 				UISprite tierSprite = NGUITools.AddSprite(badgeGUISpawnBase, badgeExtraAtlas, originalTier.spriteName);
 				tierSprite.transform.localPosition = new Vector3(position.x + 40f, position.y - 40f, -1.1f);
 				tierSprite.transform.localScale = new Vector3(39f, 50f, 1f);
@@ -169,26 +152,26 @@ public class BadgeGUI : MonoBehaviour {
 				LeanTween.moveLocal(tierSprite.gameObject, new Vector3(600f, 200f, -1.1f), 0.4f);
 				LeanTween.scale(tierSprite.gameObject, new Vector3(170f, 250, 0f), 0.4f);
 			}
-	
-			OpenDescription();
 
+			// Show description panel
+			descriptionObject.GetComponent<MoveTweenToggle>().Show();
 		}
 		else{
 			Debug.LogError("No Metadata attached on badge");
 		}
 	}
 
-	public void OpenDescription(){
-		descriptionObject.GetComponent<MoveTweenToggle>().Show();
-	}
-
 	public void CloseDescription(){
 		descriptionObject.GetComponent<MoveTweenToggle>().Hide();
 		badgeGUISpawnBase.transform.DestroyChildren();
-
 		Destroy(bgPanel);
 
+		// Enable back button for badge board
 		BadgeBoardClicked();
+	}
+
+	public void DisableBackButton(){
+		isActive = false;
 	}
 
 	public void BadgeBoardClicked(){

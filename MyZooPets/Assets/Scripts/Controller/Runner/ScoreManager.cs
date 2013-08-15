73c746eba1ea717bbb81a9afe0a90878a38f3bfa
MintyAnt <scduane@gmail.com>
2013-08-15 15:41:48 -0400

    Added coin and distance meters. Added labels for each. Changed score to be based off distance and items. Coins add coin values and not score now. Added ways to tweak distance per point n stuff. Also attempted to fix a few player bugs

--- MyZooPets/Assets/Scripts/Controller/Runner/ScoreManager.cs
+++ MyZooPets/Assets/Scripts/Controller/Runner/ScoreManager.cs
@@ -2,28 +2,58 @@
 using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
-
+    public int ScorePerIncrement = 10;
+    public float ScoreDistance = 10.0f;
     public UILabel ScoreLabel = null;
+    public UILabel CoinLabel = null;
+    public UILabel DistanceLabel = null;
 
+    private int mPlayerDistancePoints = 0;
+    private int mPlayerCoins = 0;
     private int mPlayerPoints = 0;
+    private PlayerRunner mPlayerRunner;
 
 	// Use this for initialization
-    void Start()
-    {
-        AddPoints(0);
+    void Start() {
+        GameObject playerObject = GameObject.Find("Player");
+        if (playerObject != null)
+            mPlayerRunner = playerObject.GetComponent<PlayerRunner>();
+
+        AddCoins(0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
+        if (mPlayerRunner != null) {
+            float distanceTraveled = mPlayerRunner.transform.position.z;
+
+            if (ScoreLabel != null) {
+                int newDistanceScore = (int)(distanceTraveled / ScoreDistance) * ScorePerIncrement;
+                SetDistancePoints(newDistanceScore);
+            }
+
+            if (DistanceLabel != null) {
+                DistanceLabel.text = "Distance: " + distanceTraveled;
+            }
+        }
 	    
 	}
 
+    public void AddCoins(int inNumCoinsToAdd) {
+        mPlayerCoins += inNumCoinsToAdd;
+        if (CoinLabel != null)
+            CoinLabel.text = "Coins: " + mPlayerCoins;
+    }
+
     public void AddPoints(int inNumPointsToAdd) {
         mPlayerPoints += inNumPointsToAdd;
-        Debug.Log("New score: " + mPlayerPoints);
         if (ScoreLabel != null)
-        {
-            ScoreLabel.text = "Score: " + mPlayerPoints;
-        }
+            ScoreLabel.text = "Score: " + (mPlayerDistancePoints + mPlayerPoints);
+    }
+
+    public void SetDistancePoints(int inDistancePoints) {
+        mPlayerDistancePoints = inDistancePoints;
+        if (ScoreLabel != null)
+            ScoreLabel.text = "Score: " + (mPlayerDistancePoints + mPlayerPoints);
     }
 }

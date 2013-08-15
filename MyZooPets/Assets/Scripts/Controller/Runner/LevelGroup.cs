3934b93b6cdf8f6376a7ba6d2de3f60dd7d44750
MintyAnt <scduane@gmail.com>
2013-08-15 18:19:34 -0400

    Added group levels. Pushed the parralaxing stuff into group levels. Manager talks to group levels now. Able to transition from one level to another, based off a current level group number, when a certain time has been reached.

---
+++ MyZooPets/Assets/Scripts/Controller/Runner/LevelGroup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGroup : MonoBehaviour {
+    public string LevelID = "";
+    public int LevelGroupNumber = 0;
+    public ParallaxingBackgroundGroup ParallaxingBackground;
+    public LevelComponent StartingLevelComponent;
+    public List<LevelComponent> LevelComponents;
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}

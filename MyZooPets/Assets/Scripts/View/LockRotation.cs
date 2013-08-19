fec359013ae1fd2a03a96b13df96e7c9e7b0e4b2
Wenshiang Chung <aliken9@gmail.com>
2013-08-19 14:12:28 -0400

    Fixed shadow rotation

---
+++ MyZooPets/Assets/Scripts/View/LockRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockRotation : MonoBehaviour {
+
+	public bool lockX;
+	public float overrideX;
+
+	public bool lockY;
+	public float overrideY;
+
+	public bool lockZ;
+	public float overrideZ;
+
+	void Update(){
+		if(lockX || lockY || lockZ){
+			float xRotation = lockX ? overrideX : gameObject.transform.eulerAngles.x;
+			float yRotation = lockY ? overrideY : gameObject.transform.eulerAngles.y;
+			float zRotation = lockZ ? overrideZ : gameObject.transform.eulerAngles.z;
+
+			gameObject.transform.eulerAngles = new Vector3(xRotation, yRotation, zRotation);
+		}
+	}
+}
--- MyZooPets/Assets/Scripts/View/ObjectLookUp.cs
+++ MyZooPets/Assets/Scripts/View/ObjectLookUp.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class ObjectLookUp : MonoBehaviour {
 	// Update is called once per frame
-	void Update () {
-		gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(0, 1, 0));
+	void Update(){
+		Vector3 eulerAngles = gameObject.transform.localEulerAngles;
+		//gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(0, 1, 0));
+		gameObject.transform.rotation = Quaternion.Euler( new Vector3(eulerAngles.x,Quaternion.LookRotation(new Vector3(0, 1, 0)).y, eulerAngles.z));
 	}
 }

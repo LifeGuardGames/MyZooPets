91e0178148ab78817fca225a3b50cc96cdb47107
Wenshiang Chung <aliken9@gmail.com>
2013-08-22 15:19:40 -0400

    Inhaler game camera change positioning

--- MyZooPets/Assets/Scripts/InhalerGame/InhalerUtility.cs
+++ MyZooPets/Assets/Scripts/InhalerGame/InhalerUtility.cs
@@ -9,11 +9,14 @@ public class InhalerUtility{
 
     public static bool IsTouchingObject(Touch touch, GameObject target, int maskLayer){
         Ray ray = Camera.main.ScreenPointToRay(touch.position);
+		Debug.DrawRay(ray.origin, ray.direction * 100);
         RaycastHit hit ;
         bool retVal = false;
         if (Physics.Raycast (ray, out hit, 100, maskLayer)) {
             if(hit.collider.gameObject == target){
-                retVal = true;;
+				Debug.Log ("COLLIDED WITH" + target.name);
+				retVal = true;
+
             }
         }
         return retVal;

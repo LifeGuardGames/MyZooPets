7e78a5f1fc6fff56bbc99cc4bae611c2683b027a
MintyAnt <scduane@gmail.com>
2013-08-26 14:46:50 -0400

    Added a hopefully helpful description to all major runner scripts

--- MyZooPets/Assets/Scripts/Background/ParallaxingBackground.cs
+++ MyZooPets/Assets/Scripts/Background/ParallaxingBackground.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/* Sean Duane
+ * ParallaxingBackground.cs
+ * 8:26:2013   14:43
+ * Description:
+ * Generally just a mesh with a scrollspeed.
+ */
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 

3565633370558affc1a291f7a4cb1819e2266951
Wenshiang Chung <aliken9@gmail.com>
2013-08-26 17:29:02 -0400

    Inventory texture naming fix , font filtering changed , new arrows

--- MyZooPets/Assets/Scripts/Model/Items.cs
+++ MyZooPets/Assets/Scripts/Model/Items.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 //Item Model
 //Contains: 1. name
-//          2. texture
+//          2. textureName
 //          3. cost
 //          4. ID
 
 [System.Serializable]
 public class Item {
 	public string name; //name of item
-	public Texture2D texture; //texture of item
+	public string textureName; //name of texture in the atlas
 	public int cost; //cost of item
 	public ItemCategory category; //item category
 	public ItemReceiver itemreceiver; //who will be receiving the time
 	public string description;
 	
-	public Item(string n,int c, Texture2D t, ItemCategory i)
+	public Item(string n, int c, string t, ItemCategory i)
 	{
 		this.name = n;
 		this.cost = c;
-		this.texture = t;
+		this.textureName = t;
 		this.category = i;
 	}
 }

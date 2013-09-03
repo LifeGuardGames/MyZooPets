c97f177a379aa79e9cab4ea18e84317d851491c1
jlavoine <jlavoine@Joseph-Lavoines-MacBook-Pro.local>
2013-09-03 13:46:58 -0400

    Deco System I
    The basics of the decoration system.  Entering edit mode, selecting
    nodes, scrolling through decoration UI, placing a decoration, removing
    a decoration.

--- MyZooPets/Assets/Scripts/View/EditDecosUIManager.cs
+++ MyZooPets/Assets/Scripts/View/EditDecosUIManager.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+//---------------------------------------------------
+// EditDecosUIManager()
+// This UI manager is responisble for the edit
+// decoration mode/system.
+//---------------------------------------------------
+
 public class EditDecosUIManager : SingletonUI<EditDecosUIManager> {	
-	// the exit button
+	// the exit button for leaving edit mode
 	public MoveTweenToggle tweenExit;
 	
 	// the choose deco panel
 	public GameObject goChoosePanel;
 	public ChooseDecorationUI scriptChooseUI;
 	
+	//---------------------------------------------------
+	// _OpenUI()
+	//---------------------------------------------------	
 	protected override void _OpenUI(){
 		//Hide other UI objects
 		NavigationUIManager.Instance.HidePanel();
@@ -18,7 +27,10 @@ public class EditDecosUIManager : SingletonUI<EditDecosUIManager> {
 		// show the exit button
 		tweenExit.Show();		
 	}
-
+	
+	//---------------------------------------------------
+	// _CloseUI()
+	//---------------------------------------------------	
 	protected override void _CloseUI(){
 		// if the choose menu was open, close it
 		MoveTweenToggle tween = goChoosePanel.GetComponent<MoveTweenToggle>();
@@ -32,20 +44,35 @@ public class EditDecosUIManager : SingletonUI<EditDecosUIManager> {
 		tweenExit.Hide();	
 	}
 	
-	public void UpdateChooseMenu( DecorationTypes eType ) {
+	//---------------------------------------------------
+	// UpdateChooseMenu()
+	// Called when a decoration node is selected, this
+	// function kicks off the process of displaying a 
+	// menu for showing the user which decorations they
+	// may place.
+	//---------------------------------------------------	
+	public void UpdateChooseMenu( DecorationNode decoNode ) {
 		// if the menu is not showing, show it
 		MoveTweenToggle tween = goChoosePanel.GetComponent<MoveTweenToggle>();
 		if ( !tween.IsShowing() )
 			tween.Show();
 		
-		scriptChooseUI.UpdateItems( eType );
+		// update the menu based on the incoming deco node
+		scriptChooseUI.UpdateItems( decoNode );
 	}
 	
+	//---------------------------------------------------
+	// CloseChooseMenu()
+	// Closes the choose decoration menu -- note that
+	// this does not exit deco mode.
+	//---------------------------------------------------	
 	public void CloseChooseMenu() {
 		MoveTweenToggle tween = goChoosePanel.GetComponent<MoveTweenToggle>();
 		if ( !tween.IsShowing() )
 			Debug.Log("Something trying to close an already closed choose menu for deco edit.");
 		else
 			tween.Hide();
+		
+		// we possibly want to Resources.UnloadUnusedAssets() here because the menu is instantiated
 	}
 }

91264c4e5c74cecf2ff394e5086e9062d35552c3
Wenshiang Chung <aliken9@gmail.com>
2013-09-17 18:44:03 -0400

    LWF Working, some yard changes

---
+++ MyZooPets/Assets/Scripts/Camera/CameraSortMode.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSortMode : MonoBehaviour {
+	
+	public TransparencySortMode sortMode;
+	
+	void Start () {
+		gameObject.camera.transparencySortMode = sortMode;
+	}
+}
---
+++ MyZooPets/Assets/Tests/Simple.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simple : LWFObject {
+
+	void Start()
+	{
+		setLoader();
+		// #1 Show popup lwf/textures
+		Load("test2.swf/test2", "test2.swf/");
+	}
+
+
+	void setLoader()
+	{
+		LWFObject.SetLoader(
+			lwfDataLoader:(name) => {
+				TextAsset asset = Resources.Load(name) as TextAsset;
+				if (asset == null) {
+					return null;
+				}
+				return asset.bytes;
+			},
+			textureLoader:(name) => {
+				Texture2D texture = Resources.Load(name) as Texture2D;
+				if (texture == null) {
+					return null;
+				}
+				return texture;
+			}
+		);
+	}
+}

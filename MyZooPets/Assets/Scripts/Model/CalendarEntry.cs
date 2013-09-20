397a5fd8ac1aecd629dcc2d8cdbc344f943ea9f6
jlavoine <jlavoine@Joseph-Lavoines-MacBook-Pro.local>
2013-09-20 16:22:09 -0400

    Calendar fix
    Mistakenly checked in some calendar code I was just using for testing;
    reverting.

--- MyZooPets/Assets/Scripts/Model/CalendarEntry.cs
+++ MyZooPets/Assets/Scripts/Model/CalendarEntry.cs
@@ -33,8 +33,8 @@ public class CalendarEntry{
     //=====================================
 
     public CalendarEntry(){
-        this.dayTime = DosageRecord.Hit;
-        this.nightTime = DosageRecord.Miss;
+        this.dayTime = DosageRecord.Null;
+        this.nightTime = DosageRecord.Null;
         this.bonusCollectedDayTime = false;
         this.bonusCollectedNightTime = false;
     }

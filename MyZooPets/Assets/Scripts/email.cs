f971a3fbf04f95a81a827a7ab9e2305f4d48aa44
Jason Cheng <yannru.cheng@gmail.com>
2013-10-11 16:21:30 -0400

    socialnetwork plugin added

---
+++ MyZooPets/Assets/Scripts/email.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class email : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SendMail(){
+         MailMessage mail = new MailMessage();
+ 
+            mail.From = new MailAddress("jason.sonnygrease@gmail.com");
+            mail.To.Add("yannru.cheng@gmail.com");
+            mail.Subject = "Test Mail";
+            mail.Body = "This is for testing SMTP mail from GMAIL";
+ 
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new System.Net.NetworkCredential("account", "password") as ICredentialsByHost;
+            smtpServer.EnableSsl = true;
+            ServicePointManager.ServerCertificateValidationCallback = 
+                delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) 
+                    { return true; };
+            smtpServer.Send(mail);
+         Debug.Log("success");
+    }
+
+    void OnGUI(){
+     if(GUI.Button(new Rect(10f, 10f, 100f, 100f), "Send Mail")){
+        SendMail(); 
+     }
+    }
+}

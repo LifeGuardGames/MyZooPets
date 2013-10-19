41089b839eff87c3c8169e2bc361bd31faa2369a
Jason Cheng <yannru.cheng@gmail.com>
2013-10-19 14:43:06 -0400

    adding new player physics

---
+++ MyZooPets/Assets/Scripts/Runner/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : Singleton<PlayerController> {
+    
+    // Player Handling
+    public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+    public float jumpHeight = 12;
+    
+    private float currentSpeed;
+    private float targetSpeed;
+    private Vector2 amountToMove;
+    
+    private PlayerPhysics playerPhysics;
+    private int numOfLayerToPassThrough = 0;
+
+    void Start () {
+        playerPhysics = GetComponent<PlayerPhysics>();
+    }
+    
+    void Update () {
+        targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        // targetSpeed = speed;
+        currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, acceleration);
+
+#if UNITY_EDITOR
+        if(Input.GetKey("up")) Jump();
+        if(Input.GetKey("down")) Drop();
+#endif
+
+        if(playerPhysics.Grounded && !playerPhysics.Jumping)
+            amountToMove.y = 0;
+        
+        amountToMove.x = currentSpeed;
+        amountToMove.y -= gravity * Time.deltaTime;
+        playerPhysics.Move(amountToMove * Time.deltaTime, ref numOfLayerToPassThrough);
+    }
+
+    void OnTap(TapGesture gesture) { 
+        print("tap");
+        Jump();
+    }
+
+    void OnSwipe(SwipeGesture gesture) { 
+        print("swipe");
+        FingerGestures.SwipeDirection direction = gesture.Direction;
+        if(direction == FingerGestures.SwipeDirection.Down){
+            Drop();
+        }
+    } 
+
+    private void Jump(){
+        if(playerPhysics.Grounded){
+            amountToMove.y = jumpHeight;
+            playerPhysics.Jumping = true;
+        }
+    }
+
+    private void Drop(){
+        if(playerPhysics.Grounded && !playerPhysics.Jumping)
+            // amountToMove.y = 0;
+            numOfLayerToPassThrough = 1;
+    }
+}
---
+++ MyZooPets/Assets/Scripts/Runner/PlayerPhysics.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+    private BoxCollider collider;
+    private Vector3 colliderSize;
+    private Vector3 colliderCenter;
+    private float skin = .005f; //Padding for collider
+
+    private float deltaY;
+    private float deltaX;
+    private Vector2 pos;
+    
+    public bool Grounded{get; set;} //when player touching the ground or falling
+    public bool Jumping{get; set;} //when player is jumping up. In the upward trajectory. 
+    public bool MovementStopped{get; set;}
+    
+    Ray ray;
+    RaycastHit hit;
+    
+    void Start() {
+        collider = GetComponent<BoxCollider>();
+        colliderSize = collider.size;
+        colliderCenter = collider.center;
+    }
+
+    public void Move(Vector2 moveAmount, ref int numOfLayerToPassThrough) {
+        
+        deltaY = moveAmount.y;
+        deltaX = moveAmount.x;
+        pos = transform.position;
+
+        CheckCollisionsAboveAndBelow(ref numOfLayerToPassThrough);
+        CheckCollisionsLefAndRight();
+        
+        Vector2 finalTransform = new Vector2(deltaX,deltaY);
+        transform.Translate(finalTransform);
+    }
+
+    private void CheckCollisionsAboveAndBelow(ref int numOfLayerToPassThrough){
+        Grounded = false;
+        for (int i=0; i<3; i++) {
+            float dir = Mathf.Sign(deltaY);
+            float x = (pos.x + colliderCenter.x - colliderSize.x/2) + colliderSize.x/2 * i; // Left, centre and then rightmost point of collider
+            float y = pos.y + colliderCenter.y + colliderSize.y/2 * dir; // Bottom of collider
+            ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+            Debug.DrawRay(ray.origin,ray.direction);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Abs(deltaY))){
+                // Get Distance between player and ground
+                float dst = Vector3.Distance(ray.origin, hit.point);
+
+                //when user is falling
+                if(dir == -1){
+                    //If the layer can be pass through than break and ignore collision
+                    if(hit.collider.transform.gameObject.layer == LayerMask.NameToLayer("PassThroughLayer") &&
+                        numOfLayerToPassThrough == 1){
+                        numOfLayerToPassThrough--;
+                        break;
+                    }
+
+                    // Stop player's downwards movement after coming within skin width of a collider
+                    if(dst > skin)
+                        deltaY = dst * dir - skin * dir;
+                    else
+                        deltaY = 0;
+
+                    Grounded = true;
+                    Jumping = false;
+                }else{
+                    Jumping = true;
+                }
+                break; //Break when collision is detected
+            }
+        }
+    }
+
+    private void CheckCollisionsLefAndRight(){
+        MovementStopped = false;
+        for(int i=0; i<3; i++){
+            float dir = Mathf.Sign(deltaX);
+            float x = pos.x + colliderCenter.x + colliderSize.x/2 * dir;
+            float y = pos.y + colliderCenter.y - colliderSize.y/2 + colliderSize.y/2 * i;
+
+            ray = new Ray(new Vector2(x, y), new Vector2(dir, 0));
+            Debug.DrawRay(ray.origin,ray.direction);
+            
+            if (Physics.Raycast(ray, out hit, Mathf.Abs(deltaX) + skin)) {
+                // Get Distance between player and ground
+                float dst = Vector3.Distance (ray.origin, hit.point);
+
+                //Don't allow collision for pass through layer
+                if(hit.collider.transform.gameObject.layer == LayerMask.NameToLayer("PassThroughLayer"))
+                    break;
+                
+                // Stop player's downwards movement after coming within skin width of a collider
+                if (dst > skin)
+                    deltaX = dst * dir - skin * dir;
+                else
+                    deltaX = 0;
+                
+                MovementStopped = true;
+                break; //Break when collision is detected
+            }
+        }
+    }
+}

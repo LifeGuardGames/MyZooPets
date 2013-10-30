4ec037eb79361835097e5dc762329ec9a0c7ae6a
Jason Cheng <yannru.cheng@gmail.com>
2013-10-30 12:58:15 -0400

    add memory cleaner to scene

--- MyZooPets/Assets/CUDLR/Examples/GameObjectExamples.cs
+++ MyZooPets/Assets/CUDLR/Examples/GameObjectExamples.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Net;
@@ -8,12 +9,60 @@ using System.Net;
  */
 public static class GameObjectCommands {
 
-  [CUDLR.Command("object list", "lists all the game objects in the scene")]
-  public static void ListGameObjects() {
+  [CUDLR.Command("active object list", "lists all the game objects in the scene")]
+  public static void ListActiveGameObjects() {
     UnityEngine.Object[] objects = UnityEngine.Object.FindObjectsOfType(typeof(GameObject));
     foreach (UnityEngine.Object obj in objects) {
       CUDLR.Console.Log(obj.name);
     }
+  }
+
+  [CUDLR.Command("object list", "lists all the game objects in the scene")]
+  public static void ListGameObjects(){
+    object[] gameObjects = Resources.FindObjectsOfTypeAll(typeof(GameObject));
+
+    foreach(GameObject go in gameObjects)
+      CUDLR.Console.Log(go.name);
+
+    CUDLR.Console.Log("==============Game Objects====================");
+    CUDLR.Console.Log("Count: " + gameObjects.Length);
+  }
+
+  [CUDLR.Command("texture list", "lists all the textures in the scene")]
+  public static void ListTextures(){
+    object[] textures = Resources.FindObjectsOfTypeAll(typeof(Texture));
+
+    foreach(Texture tex in textures)
+      CUDLR.Console.Log(tex.name + "--" + tex.width + "x" + tex.height);
+
+    CUDLR.Console.Log("=================Textures=====================");
+    CUDLR.Console.Log("Count: " + textures.Length);
+  }
+
+  [CUDLR.Command("material list", "lists all the materials in the scene")]
+  public static void ListMaterial(){
+    object[] materials = Resources.FindObjectsOfTypeAll(typeof(Material));
+
+    foreach(Material mat in materials)
+      CUDLR.Console.Log(mat.name);
+
+    CUDLR.Console.Log("==================Materials===================");
+    CUDLR.Console.Log("Count: " + materials.Length);
+  }
+
+  [CUDLR.Command("unload unused assets", "do what it does")]
+  public static void UnLoadAssets(){
+    Resources.UnloadUnusedAssets();
+  }
+
+  [CUDLR.Command("gc", "garbage collect")]
+  public static void GarbageCollect(){
+    System.GC.Collect();
+  }
+
+  [CUDLR.Command("total memory", "total memory from gc")]
+  public static void GetMemory(){
+    CUDLR.Console.Log("Total Memory: " + System.GC.GetTotalMemory(false));
   }
 
   [CUDLR.Command("object print", "lists properties of the object")]
@@ -35,6 +84,15 @@ public static class GameObjectCommands {
         }
       }
     }
+  }
+
+  [CUDLR.Command("load level", "load a new level")]
+  public static void LoadNewLevel(string[] args){
+    if(args.Length < 1){
+      CUDLR.Console.Log("expect a level name");
+      return;
+    }
+    Application.LoadLevel(args[0]);
   }
 }
 

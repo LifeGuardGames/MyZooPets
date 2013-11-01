d443ff12845ea2e519f658075e2ca47e70285eac
jlavoine <jlavoine@Joseph-Lavoines-MacBook-Pro.local>
2013-11-01 15:59:33 -0400

    Adding Unity_Edtior flag to store related stuff

--- MyZooPets/Assets/Plugins/StoreKit/StoreKitBinding.cs
+++ MyZooPets/Assets/Plugins/StoreKit/StoreKitBinding.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 
-#if UNITY_IPHONE
+#if UNITY_IPHONE || UNITY_EDITOR
 public class StoreKitBinding
 {
 	[DllImport("__Internal")]
--- MyZooPets/Assets/Plugins/StoreKit/StoreKitManager.cs
+++ MyZooPets/Assets/Plugins/StoreKit/StoreKitManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Prime31;
 
 
-#if UNITY_IPHONE
+#if UNITY_IPHONE || UNITY_EDITOR
 public class StoreKitManager : AbstractManager
 {
 	public static bool autoConfirmTransactions = true;

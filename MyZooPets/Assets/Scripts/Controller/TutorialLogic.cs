3b910bb5ed88b081f90ad7359a51087c6d90628c
Jason Cheng <yannru.cheng@gmail.com>
2013-11-15 15:18:09 -0500

    removing calendar tutorial

--- MyZooPets/Assets/Scripts/Controller/TutorialLogic.cs
+++ MyZooPets/Assets/Scripts/Controller/TutorialLogic.cs
@@ -3,31 +3,10 @@ using System;
 using System.Collections;
 
 public class TutorialLogic : Singleton<TutorialLogic> {
-
     //======================Events======================
     //called when any one of the tutorials is completed. UI needs to be reset 
     public static event EventHandler<EventArgs> OnTutorialUpdated;
     //==================================================
-    public bool FirstTimeCalendar{
-        get{
-            return DataManager.Instance.GameData.Tutorial.FirstTimeCalendar;
-        }
-        set{
-            DataManager.Instance.GameData.Tutorial.FirstTimeCalendar = value;
-            UpdateCallBack();
-        }
-    }
-
-    public bool FirstTimeRealInhaler{
-        get{
-            return DataManager.Instance.GameData.Tutorial.FirstTimeRealInhaler;
-        }
-        set{
-            DataManager.Instance.GameData.Tutorial.FirstTimeRealInhaler = value;
-            UpdateCallBack();
-        }
-    }
-
     public bool FirstTimeDegradTrigger{
         get{
             return DataManager.Instance.GameData.Tutorial.FirstTimeDegradTrigger;
@@ -42,14 +21,4 @@ public class TutorialLogic : Singleton<TutorialLogic> {
         if(D.Assert(OnTutorialUpdated != null, "OnTutorialUpdated has no listeners"))
             OnTutorialUpdated(null, EventArgs.Empty);
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }

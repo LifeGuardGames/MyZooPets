fa8c27b758f72ffd65170fdb7165a047a8c74ce1
Wenshiang Chung <aliken9@gmail.com>
2013-12-07 15:15:48 -0500

    Add child editor script

---
+++ MyZooPets/Assets/Editor/EditorCreateEmptyInParent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Editor create empty in parent.
+/// Editor Script: Creates an empty child in the selected parent with default transform and inherit parent layer
+/// </summary>
+public class EditorCreateEmptyInParent : MonoBehaviour {
+	
+	[MenuItem ("GameObject/Create Empty in Parent %#c")]
+	static void CreateEmptyInParent(){
+		GameObject parent = Selection.activeTransform.gameObject;
+		GameObject go = new GameObject();
+		Transform t = go.transform;
+		t.parent = parent.transform;
+		t.localPosition = Vector3.zero;
+		t.localRotation = Quaternion.identity;
+		t.localScale = Vector3.one;
+		go.layer = parent.layer;
+	}
+	
+	[MenuItem ("GameObject/Create Empty in Parent %#c", true)]
+	static bool ValidateCreateEmptyInParent(){
+		return Selection.activeTransform != null;
+	}
+}

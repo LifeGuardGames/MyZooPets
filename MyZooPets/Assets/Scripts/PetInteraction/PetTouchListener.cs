52a96f7615ba913a18fc00928119d9f5fd8a848c
Jason Cheng <yannru.cheng@gmail.com>
2013-12-09 17:03:34 -0500

    added interaction anim and listener to Pet_LWF

---
+++ MyZooPets/Assets/Scripts/PetInteraction/PetTouchListener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetTouchListener : MonoBehaviour {
+    public PetAnimator petAnimator;
+
+    void OnTap(TapGesture gesture){
+        string colliderName = gesture.Hit.collider.name;
+        if(colliderName == "Pet_LWF"){
+            petAnimator.PlayRestrictedAnim("Poke");
+        }
+    } 
+}

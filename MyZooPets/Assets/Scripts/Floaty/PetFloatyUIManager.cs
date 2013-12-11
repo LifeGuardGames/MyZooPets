ecf6718437df4a3b9e015b162855147fdaa0465d
Jason Cheng <yannru.cheng@gmail.com>
2013-12-11 14:30:40 -0500

    added floaty text to pet

--- MyZooPets/Assets/Scripts/Floaty/PetFloatyUIManager.cs
+++ MyZooPets/Assets/Scripts/Floaty/PetFloatyUIManager.cs
@@ -3,12 +3,37 @@ using System.Collections;
 
 public class PetFloatyUIManager : Singleton<PetFloatyUIManager> {
     public GameObject petFloatyPosition;
-     
-    public void MoodFloaty(string deltaMood){
+
+    public void CreateMoodFloaty(int deltaMood){
+        CreateFloaty(deltaMood, "iconHunger");
+    }
+
+    public void CreateHealthFloaty(int deltaHealth){
+        CreateFloaty(deltaHealth, "iconHeart");
+    }
+
+    public void CreatePointsFloaty(int deltaPoints){
+        CreateFloaty(deltaPoints, "iconStar");
+    }
+
+    //-------------------------------------------------------
+    // CreateFloaty()
+    // Use the FloatyUtil class to spawn the floaty image text
+    // on top of the pet's head
+    //-------------------------------------------------------
+    private void CreateFloaty(int deltaValue, string spriteName){
+        string strDeltaValue = "";
+
+        if(deltaValue > 0)
+            strDeltaValue = "+" + deltaValue;
+        else
+            strDeltaValue = "" + deltaValue;
+
         Hashtable option = new Hashtable();
         option.Add("parent", petFloatyPosition);
-        option.Add("text", deltaMood);
-        option.Add("spriteName", "iconHunger");
+        option.Add("text", strDeltaValue);
+        option.Add("spriteName", spriteName);
+
         FloatyUtil.SpawnStatsFloatyImageText(option);
     }
 

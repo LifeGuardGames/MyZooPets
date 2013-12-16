2ab213b0a5c765805b04726e2bb7b54a1fdcff6b
Jason Cheng <yannru.cheng@gmail.com>
2013-12-16 00:23:30 -0500

    integrate more runner items in the game. spawn chance for runner items need to be more frequent. barely see any special items right now

---
+++ MyZooPets/Assets/Scripts/Runner/Items/ExtraCoinsItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExtraCoinsItem : RunnerItem {
+    public int coinValue = 0; //how many coins this item is worth
+
+    // Use this for initialization
+    public override void Start() {
+        base.Start();
+    }
+    
+    // Update is called once per frame
+    public override void Update() {
+        base.Update();
+    }
+
+    public override void OnPickup() {
+        ScoreManager.Instance.AddCoins(coinValue);
+        
+        GameObject.Destroy(gameObject);
+    }
+}

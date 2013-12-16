9eb851f544a4ee5590e1e6e3241ef37bfd6d44eb
aliken9 <aliken9@gmail.com>
2013-12-15 23:29:42 -0500

    Spring system ready for integration in clinic

---
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/MultiAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiAnimationController : MonoBehaviour {
+	
+	public Animation[] animationList;
+	public bool isDebug;
+	
+	public void PlayAnimations(){
+		foreach(Animation animation in animationList){
+			animation.Play();
+		}
+	}
+	
+	void OnGUI(){
+		if(isDebug){
+			if(GUI.Button(new Rect(100, 100, 100, 100), "Play Animations")){
+				PlayAnimations();
+			}
+		}
+	}
+}

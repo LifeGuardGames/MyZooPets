3a48b58f02134e0da75ac207814880a1c6711dfd
Jason Cheng <yannru.cheng@gmail.com>
2013-12-31 12:52:33 -0500

    ZeroHealth notification listener added to stats manager

---
+++ MyZooPets/Assets/Scripts/View/ZeroHealthNotificationListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ZeroHealthNotificationListener : MonoBehaviour {
+    public int hospitalBillCost = 300;
+
+	// Use this for initialization
+	void Start () {
+        StatsController.OnZeroHealth += OnZeroHealthNotification;
+	}
+
+    void OnDestroy(){
+        StatsController.OnZeroHealth -= OnZeroHealthNotification;    
+    }
+
+    private void OnZeroHealthNotification(object sender, EventArgs args){
+        PopupNotificationNGUI.HashEntry button1Function = delegate(){
+            StatsController.Instance.ChangeStats(0, Vector3.zero, -1 * hospitalBillCost, 
+                Vector3.zero, 100, Vector3.zero, -30, Vector3.zero, true, bFloaty: false);
+        };
+        string message = StringUtils.Replace(Localization.Localize("ZERO_HEALTH"), 
+            StringUtils.NUM, hospitalBillCost.ToString());
+
+        Hashtable notificationEntry = new Hashtable();
+        notificationEntry.Add(NotificationPopupFields.Type, NotificationPopupType.OneButton);
+        notificationEntry.Add(NotificationPopupFields.Message, message); 
+        notificationEntry.Add(NotificationPopupFields.Button1Callback, button1Function); 
+        
+        NotificationUIManager.Instance.AddToQueue(notificationEntry);
+    }
+}

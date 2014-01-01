8acb4a3feb359e91960a99c65d0e76dec61d0152
Jason Cheng <yannru.cheng@gmail.com>
2014-01-01 18:46:03 -0500

    night day effect in yard

--- MyZooPets/Assets/Scripts/View/SkyboxDayNightSwitch.cs
+++ MyZooPets/Assets/Scripts/View/SkyboxDayNightSwitch.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
-public class SkyboxDayNightSwitch : Singleton<SkyboxDayNightSwitch> {
+public class SkyboxDayNightSwitch : MonoBehaviour{
 	
 	public bool isDayTime = true;
 	public Color dayColor = new Color(167f/255f, 219f/255f, 255f/255f);
 	public Color nightColor = new Color(46f/255f, 42f/255f, 92f/255f);
 
-	public void ActivateDay(){
+	void Start(){
+		CheckTime();
+	}
+
+	void OnApplicationPause(bool isPaused){
+		if(!isPaused)
+			CheckTime();
+	}
+
+	private void CheckTime(){
+		DateTime now = LgDateTime.GetTimeNow();
+		if(now.Hour	< 12)
+			ActivateDay();
+		else
+			ActivateNight();
+	}
+
+	private void ActivateDay(){
 		isDayTime = true;
 		Camera.main.backgroundColor = dayColor;
 	}
 	
-	public void ActivateNight(){
+	private void ActivateNight(){
 		isDayTime = false;
 		Camera.main.backgroundColor = nightColor;
 	}

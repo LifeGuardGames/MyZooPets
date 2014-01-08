16ac43be1171501ba7d4a60d248874927761c205
jlavoine <jlavoine@Joseph-Lavoines-MacBook-Pro.local>
2014-01-08 09:11:10 -0500

    Debug messages for wellapad refresh on suspended test bug

--- MyZooPets/Assets/Scripts/Model/WellapadData.cs
+++ MyZooPets/Assets/Scripts/Model/WellapadData.cs
@@ -26,6 +26,7 @@ public class WellapadData {
 	// when the missions have expired.
 	//---------------------------------------------------		
 	public void ResetMissions() {
+		Debug.Log("Resetting missions in the wellapad");
 		CurrentTasks = new Dictionary< string, Mission >();
 	}
 	

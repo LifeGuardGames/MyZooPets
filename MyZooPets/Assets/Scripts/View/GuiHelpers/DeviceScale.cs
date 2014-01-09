499ec6fb0ec8c5c57819cb1fbccea162a518b84e
Wenshiang Chung <aliken9@gmail.com>
2014-01-09 18:49:08 -0500

    dirty fix for blood panel

---
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/DeviceScale.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Scales gameobjects for recognizing different devices
+// Since NGUI does its own scaling, we just do it by eye on the object rather than calculated
+public class DeviceScale : MonoBehaviour {
+	
+	public Vector3 iPhone5Scale;
+	
+	void Start () {
+		if(iPhone.generation == iPhoneGeneration.iPhone5 || (Screen.width == 1136 && Screen.height == 640)){
+			gameObject.transform.localScale = iPhone5Scale;
+		}
+	}
+}

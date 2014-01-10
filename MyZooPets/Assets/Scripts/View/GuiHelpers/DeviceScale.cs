95e438a21db84d457c9e641558d4535aaf8eb799
Wenshiang Chung <aliken9@gmail.com>
2014-01-10 16:05:15 -0500

    dynamic resolution ngui calculation

--- MyZooPets/Assets/Scripts/View/GuiHelpers/DeviceScale.cs
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/DeviceScale.cs
@@ -4,16 +4,7 @@ using System.Collections;
 // Scales gameobjects for recognizing different devices
 // Since NGUI does its own scaling, we just do it by eye on the object rather than calculated
 public class DeviceScale : MonoBehaviour {
-	
-	public Vector3 iPhone5Scale;
-	public Vector3 iPhone4Scale;
-	
 	void Start () {
-		if(iPhone.generation == iPhoneGeneration.iPhone5 || (Screen.width == 1136 && Screen.height == 640)){
-			gameObject.transform.localScale = iPhone5Scale;
-		}
-		if(iPhone.generation == iPhoneGeneration.iPhone4 || iPhone.generation == iPhoneGeneration.iPhone4S || (Screen.width == 960 && Screen.height == 640)){
-			gameObject.transform.localScale = iPhone4Scale;
-		}
+		gameObject.transform.localScale = new Vector3((float)CameraManager.Instance.GetNativeWidth(), 768f, 1f);
 	}
 }

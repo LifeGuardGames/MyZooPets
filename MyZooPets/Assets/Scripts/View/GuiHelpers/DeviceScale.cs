0ed2170da163a693aadd23e4a4fc6b84d43517ec
Wenshiang Chung <aliken9@gmail.com>
2014-01-10 15:56:59 -0500

    iphone 4 resolution

--- MyZooPets/Assets/Scripts/View/GuiHelpers/DeviceScale.cs
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/DeviceScale.cs
@@ -6,10 +6,14 @@ using System.Collections;
 public class DeviceScale : MonoBehaviour {
 	
 	public Vector3 iPhone5Scale;
+	public Vector3 iPhone4Scale;
 	
 	void Start () {
 		if(iPhone.generation == iPhoneGeneration.iPhone5 || (Screen.width == 1136 && Screen.height == 640)){
 			gameObject.transform.localScale = iPhone5Scale;
+		}
+		if(iPhone.generation == iPhoneGeneration.iPhone4 || iPhone.generation == iPhoneGeneration.iPhone4S || (Screen.width == 960 && Screen.height == 640)){
+			gameObject.transform.localScale = iPhone4Scale;
 		}
 	}
 }

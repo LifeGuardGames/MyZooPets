c9d3bfb190d96ef1c172381a3563f56ce805dce3
jlavoine <jlavoine@Joseph-Lavoines-MacBook-Pro.local>
2014-01-14 15:29:09 -0500

    Runner game: Adding new component to City

--- MyZooPets/Assets/Scripts/Minigames/MinigameManager.cs
+++ MyZooPets/Assets/Scripts/Minigames/MinigameManager.cs
@@ -90,7 +90,7 @@ public abstract class MinigameManager<T> : Singleton<T> where T : MonoBehaviour 
 	private MinigameStates eCurrentState = MinigameStates.Opening;
 	private void SetGameState( MinigameStates eNewState ) {
 		if ( eCurrentState == eNewState ) {
-			Debug.LogError("Minigame is getting set to a state it's already at: " + eNewState);
+			Debug.LogError("Minigame(" + GetMinigameKey() + ") is getting set to a state it's already at: " + eNewState);
 			return;
 		}
 		

9092c4c870e4ceccb966eb065bfeac91c72341b3
Wenshiang Chung <aliken9@gmail.com>
2014-02-08 12:53:19 -0500

    egg sound pushed

---
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/AnimationSoundController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Animation sound controller.
+/// Used for event callbacks from unity animations
+/// </summary>
+
+public class AnimationSoundController : MonoBehaviour {
+	
+	public void PlayClip(string clipName){
+		AudioManager.Instance.PlayClip(clipName);
+	}
+}

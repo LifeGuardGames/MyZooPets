476a0fe79a8f063c7ffded4e95fe95c2936b44bc
Wenshiang Chung <aliken9@gmail.com>
2014-02-20 14:43:21 -0500

    testing shader scene

---
+++ MyZooPets/Assets/ParallaxingBackgroundTEST.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParallaxingBackgroundTEST : MonoBehaviour {
+    public float ScrollSpeed = 0.05f;
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+		float currentSpeed = ScrollSpeed * Time.time;
+        renderer.material.mainTextureOffset = new Vector2(currentSpeed, 0f);
+    }
+}

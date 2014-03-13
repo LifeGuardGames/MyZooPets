fec1869bbbbb9f1a5b19e0f7819f4017345450e6
Jason Cheng <yannru.cheng@gmail.com>
2014-03-13 19:33:23 -0400

    Replace wellapad okay button with reward when all tasks have been completed. Still need more testing and hasn't done the changes to Yard yet.

---
+++ MyZooPets/Assets/Scripts/Wellapad/View/WellapadActionButtonUIController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WellapadActionButtonUIController : MonoBehaviour {
+    public GameObject backButton;
+    public GameObject rewardButton;
+
+    private string missionID = "";
+
+	// Use this for initialization
+	void Start () {
+        WellapadUIManager.Instance.OnTweenDone += OnWellapadVisible;
+	}
+
+    private void OnWellapadVisible(object sender, UIManagerEventArgs args){
+        List<string> currentMissionIDs = WellapadMissionController.Instance.GetCurrentMissions();
+
+        //since we don't really use multiple missions in the wellapad right now. there will only be
+        //one id in the list
+        missionID = currentMissionIDs[0];
+
+        //use GetMission(missionid) to get the mission data
+        Mission mission = WellapadMissionController.Instance.GetMission(missionID);
+
+        if(mission != null){
+            RewardStatuses status = mission.RewardStatus;
+
+            //if reward status is claimed or unearned, so regular back button
+            if(status == RewardStatuses.Claimed || status == RewardStatuses.Unearned)
+                ShowBackButton();
+            //if reward status is unclaimed, so reward button
+            else
+                ShowRewardButton();
+        }
+
+    }
+
+    //after reward button is clicked show animation. disable it and show regular back button
+    private void RewardButtonClicked(){
+        WellapadMissionController.Instance.ClaimReward(missionID);
+        WellapadMissionController.Instance.RefreshCheck();
+
+        ShowBackButton();
+    }
+
+    private void BackButtonClicked(){
+        WellapadUIManager.Instance.CloseUI();
+
+        HideBothButtons();
+    }
+
+    private void ShowBackButton(){
+        backButton.SetActive(true);
+        rewardButton.SetActive(false);
+    }
+
+    private void ShowRewardButton(){
+        backButton.SetActive(false);
+        rewardButton.SetActive(true);
+    }
+
+    private void HideBothButtons(){
+        backButton.SetActive(false);
+        rewardButton.SetActive(false);
+    }
+}

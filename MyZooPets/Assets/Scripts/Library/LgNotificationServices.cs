5ee646da206b847b40c8bd5cce6140a290767d0b
Jason Cheng <yannru.cheng@gmail.com>
2014-03-14 13:44:28 -0400

    Minor bug fixes to LgNotificationServices

--- MyZooPets/Assets/Scripts/Library/LgNotificationServices.cs
+++ MyZooPets/Assets/Scripts/Library/LgNotificationServices.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class LgNotificationServices{
 
     public static void ScheduleLocalNotification(string message, DateTime fireDate, string title="Wellapets"){
-#if UNITY_IPHONE 
+#if UNITY_IPHONE && !UNITY_EDITOR 
         var notif = new LocalNotification();
         notif.fireDate = fireDate;
         notif.alertAction = title;
@@ -23,23 +23,18 @@ public class LgNotificationServices{
 #endif
     }
 
-//     public static void CancelAllLocalNotifications(){
-// #if UNITY_IPHONE
-//         NotificationServices.CancelAllLocalNotifications();
-// #endif
-
-// #if UNITY_ANDROID
-// #endif 
-//     }
-
+    //removes icon badge number and removed the notifications delivered to user
     public static void RemoveIconBadgeNumber(){
-#if UNITY_IPHONE 
-        var notif = new LocalNotification();
-        notif.hasAction = false;
-        notif.applicationIconBadgeNumber = -1;
+#if UNITY_IPHONE && !UNITY_EDITOR 
+        if(NotificationServices.localNotificationCount > 0){
+            var notif = new LocalNotification();
+            notif.hasAction = false;
+            notif.applicationIconBadgeNumber = -1;
 
-        NotificationServices.PresentLocalNotificationNow(notif);
-        NotificationServices.CancelAllLocalNotifications();
+            NotificationServices.PresentLocalNotificationNow(notif);
+            NotificationServices.ClearLocalNotifications();
+            NotificationServices.CancelAllLocalNotifications();
+        }
 #endif        
     }
 }

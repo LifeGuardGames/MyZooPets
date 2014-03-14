405a74b43a7d2dfda251b61c88941ac70d5907cb
Jason Cheng <yannru.cheng@gmail.com>
2014-03-14 13:06:18 -0400

    make selection grid into a static grid

---
+++ MyZooPets/Assets/Scripts/MenuScene/SelectionManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectionManager : Singleton<SelectionManager> {
+
+    public string CurrentPetID{
+        get{
+            return DataManager.Instance.CurrentPetID;
+        }
+        set{
+            DataManager.Instance.CurrentPetID = value;
+        }
+    }
+
+    public int NumOfPets{
+        get{
+            return DataManager.Instance.NumOfPets;
+        }
+    }
+
+    //Return PetMenuInfo serialized data
+    public Dictionary<string, MutableData_PetMenuInfo> PetMenuInfo{
+        get{
+            return DataManager.Instance.MenuSceneData;
+        }
+    }
+
+    //Has a game data been loaded into DataManager. Game is only safe to 
+    //start if the data is there
+    public bool IsGameDataLoaded{
+        get{
+            return DataManager.Instance.IsGameDataLoaded();
+        }
+    }
+
+    public void LoadPetGameData(){
+        DataManager.Instance.LoadGameData();
+    }
+
+    public void RemovePetData(string petID){
+        DataManager.Instance.RemovePetData(petID);
+    }
+
+}

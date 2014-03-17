7075a2821d15058d0969d3c150e76f118c889473
Jason Cheng <yannru.cheng@gmail.com>
2014-03-17 15:16:32 -0400

    adding version manager

---
+++ MyZooPets/Assets/Scripts/VersionManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VersionManager : MonoBehaviour {
+    private static VersionManager instance;
+
+    public static VersionManager Instance{
+        get{
+            if(instance == null){
+                instance = (VersionManager) FindObjectOfType(typeof(VersionManager));
+
+                if(instance == null){
+                    GameObject go = new GameObject("_Version");
+                    DontDestroyOnLoad(go);
+                    instance = go.AddComponent<VersionManager>();
+                }
+            }
+            return instance;
+        }
+    } 
+
+    void OnAwake(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    public bool IsLite(){
+        return Constants.GetConstant<bool>("IsLiteVersion");
+    }
+}

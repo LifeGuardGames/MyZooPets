f2e5a656ddb73b0d164ea6d636520a2875f9ec29
Jason Cheng <yannru.cheng@gmail.com>
2014-04-21 22:55:13 -0400

    re balancing the gating system

--- MyZooPets/Assets/Scripts/Model/MutableData/GatingProgressData.cs
+++ MyZooPets/Assets/Scripts/Model/MutableData/GatingProgressData.cs
@@ -8,23 +8,23 @@ using System.Collections.Generic;
 //---------------------------------------------------
 
 public class GatingProgressData{
-    public Dictionary<string, int> GatingProgress {get; set;} // dictionary of gate IDs to HP remaining for the monster inside
+	public Dictionary<string, int> GatingProgress { get; set; } // dictionary of gate IDs to HP remaining for the monster inside
 	
 	//---------------------------------------------------
 	// IsActivateGate()
 	// Returns whether the incoming gate is activate (i.e.
 	// the gate is active if the player hasn't opened it).
 	//---------------------------------------------------	
-	public bool IsGateActive( string strID ) {
+	public bool IsGateActive(string strID){
 		// start off optimistic
 		bool bActive = false;
 		
 		// if the gating system is not enabled, just bail now
-		if ( !IsEnabled() )
+		if(!IsEnabled())
 			return bActive;
 		
 		// if the gate's HP is > 0, it hasn't been opened yet
-		if ( GatingProgress.ContainsKey( strID ) )
+		if(GatingProgress.ContainsKey(strID))
 			bActive = GatingProgress[strID] > 0;
 		else
 			Debug.LogError("Attempting to access a non-exitant gate from GatingProgressData: " + strID);
@@ -36,9 +36,9 @@ public class GatingProgressData{
 	// RefreshGate()
 	// Refreshes the incoming gate's HP.
 	//---------------------------------------------------	
-	public void RefreshGate( DataGate data ) {
+	public void RefreshGate(DataGate data){
 		string strID = data.GetGateID();
-		if ( GatingProgress.ContainsKey( strID ) ) {
+		if(GatingProgress.ContainsKey(strID)){
 			int nHP = data.GetMonster().GetMonsterHealth();
 			GatingProgress[strID] = nHP;
 		}
@@ -50,8 +50,8 @@ public class GatingProgressData{
 	// IsEnabled()
 	// Used for testing purposes.
 	//---------------------------------------------------	
-	public bool IsEnabled() {
-		bool bEnabled = Constants.GetConstant<bool>( "GatingEnabled" );
+	public bool IsEnabled(){
+		bool bEnabled = Constants.GetConstant<bool>("GatingEnabled");
 		return bEnabled;	
 	}
 	
@@ -60,11 +60,11 @@ public class GatingProgressData{
 	// This is probably just a temp function until the
 	// gating system gets finished.
 	//---------------------------------------------------		
-	public int GetGateHP( string strID ) {
+	public int GetGateHP(string strID){
 		int nHP = 0;
 		
 		// if the gate's HP is > 0, it hasn't been opened yet
-		if ( GatingProgress.ContainsKey( strID ) )
+		if(GatingProgress.ContainsKey(strID))
 			nHP = GatingProgress[strID];
 		else
 			Debug.LogError("Attempting to access a non-exitant gate from GatingProgressData");
@@ -72,23 +72,23 @@ public class GatingProgressData{
 		return nHP;		
 	}
 
-    //=======================Initialization==================
-    public GatingProgressData(){
-    	Init();
-    }
+	//=======================Initialization==================
+	public GatingProgressData(){
+		Init();
+	}
 
-    //Populate with dummy data
-    private void Init(){
-        GatingProgress = new Dictionary<string, int>();		
+	//Populate with dummy data
+	private void Init(){
+		GatingProgress = new Dictionary<string, int>();		
 		
 		// load all our gating data from xml
 		LoadFromXML();		
-    }
+	}
 
 	//---------------------------------------------------
 	// VersionCheck()
 	//---------------------------------------------------	
-	public void VersionCheck() {
+	public void VersionCheck(){
 		// when we are doing a version check, just load the data from xml again.
 		// any existing data will be left alone, and new data will be inserted into our dictionary.
 		LoadFromXML();	
@@ -99,17 +99,17 @@ public class GatingProgressData{
 	// Loads gating data from XML and copies it into our
 	// save data if it does not already exist.
 	//---------------------------------------------------	
-	private void LoadFromXML() {
+	private void LoadFromXML(){
 		// init the data by filling the dictionary with xml data
 		Dictionary<string, DataGate> dictGates = DataGateLoader.GetAllData();
-		foreach(KeyValuePair<string, DataGate> entry in dictGates) {
+		foreach(KeyValuePair<string, DataGate> entry in dictGates){
 			string strKey = entry.Key;
-		    DataGate dataGate = entry.Value;
+			DataGate dataGate = entry.Value;
 			int nHP = dataGate.GetMonster().GetMonsterHealth();
 			
 			// maps gate key to monster's max hp (i.e. no progress)
 			// don't map it if it already exists; it means that the data for that key was already loaded and contains mutable save data
-			if ( !GatingProgress.ContainsKey( strKey ) )
+			if(!GatingProgress.ContainsKey(strKey))
 				GatingProgress[strKey] = nHP;
 		}			
 	}

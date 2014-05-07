1d55e3cbce42a1ef74f3feba004ab78e768b6a8d
Wenshiang Chung <aliken9@gmail.com>
2014-05-07 15:46:25 -0400

    bedroomInhalerUIManager added to control the appearance of the state of the inhaler in the bedroom

---
+++ MyZooPets/Assets/Scripts/View/BedroomInhalerUIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BedroomInhalerUIManager : MonoBehaviour {
+
+	public Animation spinningAnimation;
+	public GameObject starParticle;
+	public GameObject rechargeParticle;
+
+	// Start the correct animations based on its state
+	void Start(){
+		if(PlayPeriodLogic.Instance.CanUseRealInhaler){
+			spinningAnimation.Play();
+			starParticle.SetActive(true);
+			rechargeParticle.SetActive(false);
+		}
+		else{
+			spinningAnimation.Stop();
+			starParticle.SetActive(false);
+			rechargeParticle.SetActive(true);
+		}
+	}
+}

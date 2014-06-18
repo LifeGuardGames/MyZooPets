5cd4ae1fc98d7a612bde4d8a08bbedc46c3e1b17
Jason Cheng <yannru.cheng@gmail.com>
2014-06-18 15:47:45 -0400

    Integrated MenuScenePetAnimation

---
+++ MyZooPets/Assets/Scripts/PetAnimations/MenuScenePetManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScenePetManager : MonoBehaviour{
+	public Animator animator;
+	private float timer = 0;
+	private float timeBeforeNextRandomAnimation = 5f;
+	private string[] animTriggers = new string[]{"jump", "wave"}; //trigger param from AnimController
+
+	// Update is called once per frame
+	void Update(){
+		timer += Time.deltaTime;
+		if(timer > timeBeforeNextRandomAnimation){
+			timer = 0;
+			int randomIndex = Random.Range(0, animTriggers.Length);
+			animator.SetTrigger(animTriggers[randomIndex]);
+		}
+	}
+}
+	

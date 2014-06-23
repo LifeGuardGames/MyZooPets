ae0673078e4d916d932be3f078eabb541aca0b8b
Wenshiang Chung <aliken9@gmail.com>
2014-06-23 15:36:11 -0400

    minipet panel set in menuscene

---
+++ MyZooPets/Assets/Scripts/View/MinipetHUDUIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinipetHUDUIManager : SingletonUI<MinipetHUDUIManager> {
+
+	protected override void _OpenUI(){
+
+		//Hide other UI objects
+		NavigationUIManager.Instance.HidePanel();
+		HUDUIManager.Instance.HidePanel();
+		EditDecosUIManager.Instance.HideNavButton();
+		InventoryUIManager.Instance.ShowPanel();
+	}
+
+	protected override void _CloseUI(){
+
+		//Show other UI Objects
+		NavigationUIManager.Instance.ShowPanel();
+		HUDUIManager.Instance.ShowPanel();
+		InventoryUIManager.Instance.ShowPanel();
+		EditDecosUIManager.Instance.ShowNavButton();
+	}
+}

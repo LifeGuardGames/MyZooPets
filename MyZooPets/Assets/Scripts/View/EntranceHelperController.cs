ad1e4c83690872b2016c2d4f6c15c17b2c5b8ea4
Wenshiang Chung <aliken9@gmail.com>
2014-07-10 16:12:24 -0400

    Entrance helper ready for jason

---
+++ MyZooPets/Assets/Scripts/View/EntranceHelperController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Entrance helper controller.
+/// This is the controller script for newly spawned entrances and objects (typically by unlocking something)
+/// </summary>
+public class EntranceHelperController : MonoBehaviour {
+	public GameObject arrowGameObject;
+	public float arrowShowDelay = 1f;
+	public ParticleSystem spawnParticle;
+
+	void Start(){
+		spawnParticle.Play();
+		arrowGameObject.SetActive(false);
+		Invoke("ShowArrow", arrowShowDelay);
+	}
+
+	public void ShowArrow(){
+		arrowGameObject.SetActive(true);
+	}
+}

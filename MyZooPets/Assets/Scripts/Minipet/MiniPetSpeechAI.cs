c9be58301cd93528492a2640bf536dfd5cc9b4fe
Jason Cheng <yannru.cheng@gmail.com>
2014-07-10 11:20:48 -0400

    start to integrate MP level up

---
+++ MyZooPets/Assets/Scripts/Minipet/MiniPetSpeechAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniPetSpeechAI : MonoBehaviour{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowDirtyMsg(){
+		Hashtable msgOption = new Hashtable();
+		msgOption.Add(PetSpeechController.Keys.MessageText, Localization.Localize("NO_FIRE_INHALER_0"));
+		msgOption.Add(PetSpeechController.Keys.ImageTextureName, "itemInhalerMain");
+		GetComponent<PetSpeechController>().Talk(msgOption);
+	}
+}
--- MyZooPets/Assets/Scripts/Speech/SpeechController.cs
+++ MyZooPets/Assets/Scripts/Speech/SpeechController.cs
@@ -5,84 +5,81 @@ using System.Collections;
 // Inherit from this class to get the speech queue 
 // functionality. 
 //---------------------------------------------------
+/// <summary>
+/// Speech controller. Inherit from this class to get the speech queue functionality
+/// </summary>
 public abstract class SpeechController<T> : Singleton<T> where T : MonoBehaviour {
 	public float speechDuration = .7f; //how long the speech bubble appears before destroyed
 	public bool isDebug = false;
 
 	private bool isActive = false; // If this is active, check things every frame
 	private Queue speechQueue = new Queue(); //queue that contains waiting messages
-	private bool qLock = false;
+	private bool isQueueLocked = false;
 
 	protected GameObject currentMessage; //current message that is displayed to the user
 
 	//-------------abstract methods----------------------
-    //---------------------------------------------------
-    // SpawnMessage()
-    // Look at the message keys and decide what is the 
-    // appropriate layout to use for this message
-    //---------------------------------------------------
+	/// <summary>
+	/// Spawns the message. Look at the message keys and decide what is the
+	/// appropriate layout to use for this message
+	/// </summary>
+	/// <param name="message">Message.</param>
 	protected abstract void SpawnMessage(Hashtable message);
 
 	void Update(){
 		if(isActive){
 			//if there are messages in the queue pop and display
-			if(speechQueue.Count >= 1 && !qLock){
+			if(speechQueue.Count >= 1 && !isQueueLocked){
 				PopQueueAndDisplay();
 			}
 
 			//If queue is empty then turn the queue to inactive
-			if(speechQueue.Count == 0 && !qLock){
+			if(speechQueue.Count == 0 && !isQueueLocked){
 				SetInactive();
 			}
 		}
 	}
 
-	//---------------------------------------------------
-	// Talk()
-	// Display text ontop of the pet
-	//---------------------------------------------------
+	/// <summary>
+	/// Talk the specified message.
+	/// </summary>
+	/// <param name="message">Message.</param>
 	public void Talk(Hashtable message){
 		SetActive();
 		speechQueue.Enqueue(message);
 	}
-
 	
-	//---------------------------------------------------
-	// SetActive()
-	// Show the components and start checking queue
-	//---------------------------------------------------
+	/// <summary>
+	/// Show the components and start checking queue
+	/// </summary>
 	private void SetActive(){
 		isActive = true;
 	}
-
-	//---------------------------------------------------
-	// SetInactive()
-	// Hide the components and stop checking queue
-	//---------------------------------------------------
+	
+	/// <summary>
+	/// Hide the components and stop checking queue.
+	/// </summary>
 	private void SetInactive(){
 		isActive = false;
 	}
 
-	//---------------------------------------------------
-	// PopQueueAndDisplay()
-	// get message hash from queue and display
-	//---------------------------------------------------
+	/// <summary>
+	/// Pops the queue and display.
+	/// </summary>
 	private void PopQueueAndDisplay(){
 		Hashtable message = (Hashtable)speechQueue.Dequeue();
 		SpawnMessage(message);
 
 		//Unlock message queue	
-		qLock = true;
+		isQueueLocked = true;
 		Invoke("UnlockQueue", speechDuration);
 	}
-
-	//---------------------------------------------------
-	// UnlockQueue()
-	// After speechDuration destroyed the spawned message
-	// and unlock message queue
-	//---------------------------------------------------
+	
+	/// <summary>
+	/// After speech duration destroyed the spawned message then Unlocks the queue.
+	/// </summary>
 	private void UnlockQueue(){
-		qLock = false;
+		isQueueLocked = false;
 
 		//Destroy message 
 		if(currentMessage != null)

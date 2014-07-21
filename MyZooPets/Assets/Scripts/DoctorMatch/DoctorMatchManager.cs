52ab8d6ccb4eb7a9e23f8e90f8cd2634c42bcda1
Wenshiang Chung <aliken9@gmail.com>
2014-07-21 19:07:12 -0400

    Doctor game update

--- MyZooPets/Assets/Scripts/DoctorMatch/DoctorMatchManager.cs
+++ MyZooPets/Assets/Scripts/DoctorMatch/DoctorMatchManager.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class DoctorMatchManager : MinigameManager<DoctorMatchManager> {
 
 	// Move this to constant XML -------
-	public float speedIncreaseInterval = 1.0f;
-
+	public float startSpeed = 2f;
+	public float speedIncreaseInterval = 0.2f;
+	public int speedUpMatchInterval = 5;	// Amount of pets correct needed before speedup occurs
+	private int speedUpMatchTrack = 0;
 	public AssemblyLineController assemblyLineController;
 
+	public Sprite[] spriteList;
 
 	private int numOfCorrectDiagnose; //keep track of the number of correct diagnose
 
@@ -38,16 +41,16 @@ public class DoctorMatchManager : MinigameManager<DoctorMatchManager> {
 	protected override void _NewGame(){
 		// set num of correct diagnose for each new game
 		numOfCorrectDiagnose = 0;
-
-
+		assemblyLineController.SetSpeed(startSpeed);
 		assemblyLineController.StartSpawning();
-
 	}
 	
 	//---------------------------------------------------
 	// _GameOver()
 	//---------------------------------------------------		
 	protected override void _GameOver(){
+		assemblyLineController.StopSpawning();
+
 		BadgeLogic.Instance.CheckSeriesUnlockProgress(BadgeType.PatientNumber, numOfCorrectDiagnose, true);
 	}
 	
@@ -75,11 +78,19 @@ public class DoctorMatchManager : MinigameManager<DoctorMatchManager> {
 		// play appropriate sound
 		AudioManager.Instance.PlayClip("clinicCorrect");
 
+		speedUpMatchTrack++;
+		if(speedUpMatchTrack == speedUpMatchInterval){
+			speedUpMatchTrack = 0;
+			SpeedGameUp();
+		}
+
 		// Analytics
 //		Analytics.Instance.DiagnoseResult(Analytics.DIAGNOSE_RESULT_CORRECT, character.GetStage(), zone.GetStage());
 	}
 
 	public void CharacterScoredWrong(){
+		Debug.Log("Wronggg");
+
 		// character was sent to wrong zone...lose a life!
 		UpdateLives(-1);
 
@@ -91,13 +102,50 @@ public class DoctorMatchManager : MinigameManager<DoctorMatchManager> {
 
 		// also slow down the game, if this didn't cause us to have a game over
 		MinigameStates eState = GetGameState();
-		if(eState == MinigameStates.Playing)
-			SlowGameDown();
 
-		Debug.Log(eState);
+		if(eState == MinigameStates.Playing){
+			speedUpMatchTrack = 0;	// Reset speed track counter
+			SlowGameDown();
+		}
+	}
+
+	private void SpeedGameUp(){
+		assemblyLineController.SetSpeed(assemblyLineController.Speed + speedIncreaseInterval);
 	}
 
 	private void SlowGameDown(){
-		
+		// Cut speed in half for now
+		assemblyLineController.SetSpeed(assemblyLineController.Speed / 2f);
+	}
+
+	public void SetUpRandomAssemblyItemSprite(GameObject assemblyLineItemObject){
+
+		AssemblyLineItem item = assemblyLineItemObject.GetComponent<AssemblyLineItem>();
+		item.SetSpeed(assemblyLineController.Speed);
+
+		SpriteRenderer sprite = assemblyLineItemObject.transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>();
+
+		int randomNum = Random.Range(1,4);
+
+		int chooseSpriteRandom = Random.Range(0,4);
+
+		switch(randomNum){
+		case 1:
+			item.itemKey = "green";
+			sprite.sprite = spriteList[chooseSpriteRandom];
+			break;
+		case 2:
+			item.itemKey = "yellow";
+			sprite.sprite = spriteList[4+chooseSpriteRandom];
+			break;
+		case 3:
+			item.itemKey = "red";
+			sprite.sprite = spriteList[8+chooseSpriteRandom];
+			break;
+		default:
+			Debug.LogError("Not valid random number");
+			break;
+		}
+//		Debug.Log(item.itemKey);
 	}
 }
--- MyZooPets/Assets/Scripts/Minigames/MinigameLife.cs
+++ MyZooPets/Assets/Scripts/Minigames/MinigameLife.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 // on the HUD of the player in a minigame.
 //---------------------------------------------------
 
-public class MinigameLife : MonoBehaviour {
+public class MinigameLife : MonoBehaviour{
 	// index of this life
 	public int nIndex;
 	
@@ -23,15 +23,19 @@ public class MinigameLife : MonoBehaviour {
 	// the state of this life
 	//private bool bOn;
 	
-	void Start() {
+	void Start(){
 		// FFFFFFUFUUUUUUUUUUU
-		if ( NinjaManager.Instance != null ) {
+		if(NinjaManager.Instance != null){
 			NinjaManager.OnNewGame += OnNewGame;
 			NinjaManager.OnLivesChanged += OnLivesChanged;			
 		}
-			else if ( DGTManager.Instance != null ) {
+		else if(DGTManager.Instance != null){
 			DGTManager.OnNewGame += OnNewGame;
 			DGTManager.OnLivesChanged += OnLivesChanged;			
+		}
+		else if(DoctorMatchManager.Instance != null){
+			DoctorMatchManager.OnNewGame += OnNewGame;
+			DoctorMatchManager.OnLivesChanged += OnLivesChanged;			
 		}
 	}
 	
@@ -40,39 +44,45 @@ public class MinigameLife : MonoBehaviour {
 	// When the user restarts the game and a new game
 	// begins.
 	//---------------------------------------------------
-	private void OnNewGame(object sender, EventArgs args) {
+	private void OnNewGame(object sender, EventArgs args){
 		// since a new game is beginning, toggle on
-		Toggle( true );
+		Toggle(true);
 	}	
 	
 	//---------------------------------------------------
 	// OnLivesChanged()
 	//---------------------------------------------------
-	private void OnLivesChanged(object sender, LivesChangedArgs args) {
+	private void OnLivesChanged(object sender, LivesChangedArgs args){
 		// get the number of lives there are
 		int nLives = GetLives();
 		
 		int nChange = args.GetChange();
-		if ( nChange < 0 && nLives+1 == nIndex ) {
+		if(nChange < 0 && nLives + 1 == nIndex){
 			// if we are LOSING a life and the current lives +1 == this life's index, it means that this life was just lost, so toggle off
-			Toggle( false );
+			Toggle(false);
 
 			// Play the camera shake animation
-			Camera.main.animation.Play();
+			if(Camera.main.animation != null){
+				Camera.main.animation.Play();
+			}
 		}
-		else if ( nChange > 0 && nLives == nIndex ) {
+		else if(nChange > 0 && nLives == nIndex){
 			// else if we are GAINING a life and the current lives == this life's index, it means this life was just gained, so toggl eon
-			Toggle( true );
+			Toggle(true);
 		}
-	}	
+	}
 	
-	private int GetLives() {
+	private int GetLives(){
 		int nLives = 0;
-		if ( NinjaManager.Instance != null )
+		if(NinjaManager.Instance != null){
 			nLives = NinjaManager.Instance.GetLives();
-		else if ( DGTManager.Instance != null )
+		}
+		else if(DGTManager.Instance != null){
 			nLives = DGTManager.Instance.GetLives();
-		
+		}
+		else if(DoctorMatchManager.Instance != null){
+			nLives = DoctorMatchManager.Instance.GetLives();
+		}
 		return nLives;
 	}
 	
@@ -80,17 +90,17 @@ public class MinigameLife : MonoBehaviour {
 	// Toggle()
 	// Turn this life on or off.
 	//---------------------------------------------------	
-	public void Toggle( bool bOn ) {
+	public void Toggle(bool bOn){
 		// cache the state of this life
 		//this.bOn = bOn;
 		
 		// change the tint based on on/off
-		Color tint = bOn ? new Color(255,255,255,255) : new Color(0,0,0,255);
+		Color tint = bOn ? new Color(255, 255, 255, 255) : new Color(0, 0, 0, 255);
 		sprite.color = tint;
 		
 		// play the particle system associated with this toggle, if it exists
 		ParticleSystemController system = bOn ? systemOn : systemOff;
-		if ( system )
+		if(system)
 			system.Play();
 	}
 }

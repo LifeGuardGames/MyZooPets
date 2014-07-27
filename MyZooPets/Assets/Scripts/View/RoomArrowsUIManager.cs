317fb74c88e9eef7f151164fd1c87f72f5911a15
Wenshiang Chung <aliken9@gmail.com>
2014-07-27 15:01:11 -0400

    room arrow on its own panel and ui manager

---
+++ MyZooPets/Assets/Scripts/View/RoomArrowsUIManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomArrowsUIManager : Singleton<RoomArrowsUIManager> {
+
+	public TweenToggleDemux roomArrowsDemux;
+	public TweenToggle leftArrowTween;
+	public TweenToggle rightArrowTween;
+
+	
+	// Shows both arrows
+	public void ShowPanel(){
+		leftArrowTween.Show();
+		rightArrowTween.Show();
+	}
+
+	// Hides both arrows
+	public void HidePanel(){
+		leftArrowTween.Hide();
+		rightArrowTween.Hide();
+	}
+
+	// Shows left arrow
+	public void ShowLeftArrow(){
+		leftArrowTween.Show();
+		rightArrowTween.Hide();
+	}
+
+	// Shows right arrow
+	public void ShowRightArrow(){
+		rightArrowTween.Show();
+		leftArrowTween.Hide();
+	}
+
+//	void OnGUI(){
+//		if(GUI.Button(new Rect(100, 100, 100, 100), "Show all")){
+//			ShowPanel();
+//		}
+//		else if(GUI.Button(new Rect(200, 100, 100, 100), "Hide all")){
+//			HidePanel();
+//		}
+//		else if(GUI.Button(new Rect(300, 100, 100, 100), "Show Left")){
+//			ShowLeftArrow();
+//		}
+//		else if(GUI.Button(new Rect(400, 100, 100, 100), "Show Right")){
+//			ShowRightArrow();
+//		}
+//	}
+}

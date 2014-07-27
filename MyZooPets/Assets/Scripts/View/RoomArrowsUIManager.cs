3e4dda3e9e9ae2be621599eb0603eddc51fe0d3c
Jason Cheng <yannru.cheng@gmail.com>
2014-07-27 17:06:15 -0400

    Implemented basic swipe arrow

--- MyZooPets/Assets/Scripts/View/RoomArrowsUIManager.cs
+++ MyZooPets/Assets/Scripts/View/RoomArrowsUIManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class RoomArrowsUIManager : Singleton<RoomArrowsUIManager> {
@@ -7,17 +8,51 @@ public class RoomArrowsUIManager : Singleton<RoomArrowsUIManager> {
 	public TweenToggle leftArrowTween;
 	public TweenToggle rightArrowTween;
 
+	void Start(){
+		CameraManager.Instance.GetPanScript().OnPartitionChanged += ShowPanel;
+		Invoke("ShowPanel", 0.5f);
+	}
+
+	void OnDestroyed(){
+		CameraManager.Instance.GetPanScript().OnPartitionChanged -= ShowPanel;
+	}
+
+	private void ShowPanel(object sender, EventArgs args){
+		ShowPanel();
+	}
 	
 	// Shows both arrows
 	public void ShowPanel(){
-		leftArrowTween.Show();
-		rightArrowTween.Show();
+		int currentPartition = CameraManager.Instance.GetPanScript().currentPartition;
+		int firstPartition = CameraManager.Instance.GetPanScript().firstPartition;
+		int lastPartition = CameraManager.Instance.GetPanScript().lastPartition;
+
+		if(currentPartition == firstPartition){
+			ShowRightArrow();
+		}
+		else if(currentPartition == lastPartition){
+			ShowLeftArrow();
+		}
+		else{
+			//check for gating
+			bool isEnabled = Constants.GetConstant<bool>("GatingEnabled");
+			bool canEnterRightRoom = GatingManager.Instance.CanEnterRoom(currentPartition, RoomDirection.Left);
+			if(!isEnabled || canEnterRightRoom)
+				ShowBothArrows();
+			else
+				ShowLeftArrow();
+		}
 	}
 
 	// Hides both arrows
 	public void HidePanel(){
 		leftArrowTween.Hide();
 		rightArrowTween.Hide();
+	}
+
+	public void ShowBothArrows(){
+		leftArrowTween.Show();
+		rightArrowTween.Show();
 	}
 
 	// Shows left arrow
@@ -32,18 +67,26 @@ public class RoomArrowsUIManager : Singleton<RoomArrowsUIManager> {
 		leftArrowTween.Hide();
 	}
 
-	void OnGUI(){
-		if(GUI.Button(new Rect(100, 100, 100, 100), "Show all")){
-			ShowPanel();
-		}
-		else if(GUI.Button(new Rect(200, 100, 100, 100), "Hide all")){
-			HidePanel();
-		}
-		else if(GUI.Button(new Rect(300, 100, 100, 100), "Show Left")){
-			ShowLeftArrow();
-		}
-		else if(GUI.Button(new Rect(400, 100, 100, 100), "Show Right")){
-			ShowRightArrow();
-		}
+	public void RightArrowClicked(GameObject sender){
+		CameraManager.Instance.GetPanScript().RightRoom();
 	}
+
+	public void LeftArrowClicked(GameObject sender){
+		CameraManager.Instance.GetPanScript().LeftRoom();
+	}
+
+//	void OnGUI(){
+//		if(GUI.Button(new Rect(100, 100, 100, 100), "Show all")){
+//			ShowPanel();
+//		}
+//		else if(GUI.Button(new Rect(200, 100, 100, 100), "Hide all")){
+//			HidePanel();
+//		}
+//		else if(GUI.Button(new Rect(300, 100, 100, 100), "Show Left")){
+//			ShowLeftArrow();
+//		}
+//		else if(GUI.Button(new Rect(400, 100, 100, 100), "Show Right")){
+//			ShowRightArrow();
+//		}
+//	}
 }

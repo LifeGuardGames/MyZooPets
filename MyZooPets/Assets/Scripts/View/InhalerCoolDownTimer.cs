54c5ae107c7705e7ac729794140a46a9d1e4d391
Jason Cheng <yannru.cheng@gmail.com>
2014-07-28 21:24:56 -0400

    1)Change text color for count down timer. 2)New layout text layout for PopupNotification_FireBlow

--- MyZooPets/Assets/Scripts/View/InhalerCoolDownTimer.cs
+++ MyZooPets/Assets/Scripts/View/InhalerCoolDownTimer.cs
@@ -17,13 +17,13 @@ public class InhalerCoolDownTimer : MonoBehaviour {
 		TimeSpan timeLeft = args.TimeLeft;
 		string displayTime = "";
 		
-		if(timeLeft.Hours > 0)
-			displayTime = string.Format("{0}[FFFF33]h[-] {1}[FFFF33]m[-] {2}[FFFF33]s[-]", 
+		if(timeLeft.Hours > 0) 
+			displayTime = string.Format("{0}[f4a460]h[-] {1}[f4a460]m[-] {2}[f4a460]s[-]", 
 			                            timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
 		else if(timeLeft.Minutes > 0)
-			displayTime = string.Format("{0}[FFFF33]m[-] {1}[FFFF33]s[-]", timeLeft.Minutes, timeLeft.Seconds);
+			displayTime = string.Format("{0}[f4a460]m[-] {1}[f4a460]s[-]", timeLeft.Minutes, timeLeft.Seconds);
 		else
-			displayTime = string.Format("{0}[FFFF33]s[-]", timeLeft.Seconds);
+			displayTime = string.Format("{0}[f4a460]s[-]", timeLeft.Seconds);
 		
 		
 		// set the label

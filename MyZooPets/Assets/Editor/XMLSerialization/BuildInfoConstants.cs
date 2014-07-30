0be34fa2a25eaca8f34310095c1ac638e9c56fa0
Jason Cheng <yannru.cheng@gmail.com>
2014-07-30 12:22:30 -0400

    getting build ready for aetna

---
+++ MyZooPets/Assets/Editor/XMLSerialization/BuildInfoConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Xml; 
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[XmlRoot("Constants")]
+public class BuildInfoConstants{
+	[XmlElement("Constant")]
+	public List<InfoConstant> BuildSettingConstantList = new List<InfoConstant>();
+}

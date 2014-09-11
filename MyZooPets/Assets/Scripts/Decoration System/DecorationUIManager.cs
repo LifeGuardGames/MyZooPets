f296c019b282b95f6efc0d13241f42bec379806f
Wenshiang Chung <aliken9@gmail.com>
2014-09-11 18:22:38 -0400

    Decoration overhaul WIP1

---
+++ MyZooPets/Assets/Scripts/Decoration System/DecorationUIManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class DecorationUIManager : SingletonUI<DecorationUIManager> {
+
+	public EventHandler<EventArgs> OnDecoPickedUp;   // when a decoration is picked up
+	public EventHandler<EventArgs> OnDecoDropped;   // when a decoration is picked up
+
+	private bool isActive = false;
+
+	public GameObject backButton;
+	public DecorationItem currentDeco;
+
+	//When the highscore board is clicked and zoomed into
+	protected override void _OpenUI(){
+		if(!isActive){
+			isActive = true;
+
+			this.GetComponent<TweenToggleDemux>().Show();
+
+			//Hide other UI objects
+			NavigationUIManager.Instance.HidePanel();
+			HUDUIManager.Instance.HidePanel();
+			InventoryUIManager.Instance.HidePanel();
+			EditDecosUIManager.Instance.HideNavButton();
+			RoomArrowsUIManager.Instance.HidePanel();
+
+			backButton.SetActive(true);
+		}
+	}
+	
+	//The back button on the left top corner is clicked to zoom out of the highscore board
+	protected override void _CloseUI(){
+		if(isActive){
+			isActive = false;
+
+			this.GetComponent<TweenToggleDemux>().Hide();
+			
+			//Show other UI Objects
+			NavigationUIManager.Instance.ShowPanel();
+			HUDUIManager.Instance.ShowPanel();
+			InventoryUIManager.Instance.ShowPanel();
+			EditDecosUIManager.Instance.ShowNavButton();
+			RoomArrowsUIManager.Instance.ShowPanel();
+			
+			backButton.SetActive(false);
+		}
+	}
+}
---
+++ MyZooPets/Assets/Scripts/Decoration System/DecorationZone.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+/// <summary>
+/// Decoration zone.
+/// This is the replacement for decoration nodes in V1.3.4 update
+/// This is the zone that the inventory will be dragged and dropped on.
+/// </summary>
+
+public enum DecorationZoneState{
+	Neutral, Inactive, Active, Hover
+}
+
+public class DecorationZone : MonoBehaviour {
+
+	// what type of decorations can go on this node?
+	public DecorationTypes nodeType;
+	public DecorationTypes GetDecoType(){
+		return nodeType;
+	}
+
+	public UISprite spriteOutline;
+	public UISprite spriteFill;
+	public Color neutralColorOutline;
+	public Color neutralColorFill;
+	public Color activeColorOutline;
+	public Color activeColorFill;
+	public Color inactiveColorOutline;
+	public Color inactiveColorFill;
+
+	public Animation activeHoverAnimation;
+
+	private bool isHovered = false;
+	private bool isHoverPlaying = false;
+
+	private DecorationZoneState state;
+
+	void Start(){
+		DecorationUIManager.Instance.OnDecoPickedUp += OnDecorationPickedUp;
+		DecorationUIManager.Instance.OnDecoDropped += OnDecorationDropped;
+	}
+
+	void OnDestroy(){
+		DecorationUIManager.Instance.OnDecoPickedUp -= OnDecorationPickedUp;
+		DecorationUIManager.Instance.OnDecoDropped -= OnDecorationDropped;
+	}
+
+	/// <summary>
+	/// Raises the decoration picked up event.
+	/// </summary>
+	private void OnDecorationPickedUp(object sender, EventArgs args){
+		SetState(DecorationUIManager.Instance.currentDeco.DecorationType.ToString());
+	}
+
+	/// <summary>
+	/// Raises the decoration dropped event.
+	/// </summary>
+	private void OnDecorationDropped(object sender, EventArgs args){
+		SetState(null);
+	}
+
+	/// <summary>
+	/// Raycasting call from drag object
+	/// </summary>
+	public void CheckHover(GameObject sender){
+		SetState(DecorationUIManager.Instance.currentDeco.DecorationType.ToString(), isHovered:true);
+	}
+
+	/// <summary>
+	/// Sets the state of the node.
+	/// NOTE: Only for object picked up! hovering will be done with raycasting
+	/// </summary>
+	/// <param name="typeName">Type name.</param>
+	private void SetState(string nodeTypeName, bool isHovered = false){
+		// Neutral state, play idle state
+		if(nodeTypeName == null){
+			state = DecorationZoneState.Neutral;
+
+			isHoverPlaying = false;
+			if(!isHoverPlaying){
+				animation.Stop();
+			}
+
+			spriteOutline.color = neutralColorOutline;
+			spriteFill.color = neutralColorFill;
+		}
+		// Wrong state, play the inactive state
+		else if(nodeTypeName != nodeType.ToString()){
+			state = DecorationZoneState.Inactive;
+
+			isHoverPlaying = false;
+			if(!isHoverPlaying){
+				animation.Stop();
+			}
+
+			spriteOutline.color = inactiveColorOutline;
+			spriteFill.color = inactiveColorFill;
+		}
+		// Correct state, is hovered with right object
+		else if(state == DecorationZoneState.Active && isHovered){
+			state = DecorationZoneState.Hover;
+
+			if(!isHoverPlaying){
+				isHoverPlaying = true;
+				activeHoverAnimation.Play();
+			}
+		}
+		// Correct state, play the active state
+		else{
+			state = DecorationZoneState.Active;
+
+			isHoverPlaying = false;
+			if(!isHoverPlaying){
+				animation.Stop();
+			}
+
+			spriteOutline.color = activeColorOutline;
+			spriteFill.color = activeColorFill;
+		}
+	}
+}

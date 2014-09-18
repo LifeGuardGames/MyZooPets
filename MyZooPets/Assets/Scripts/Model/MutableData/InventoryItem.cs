9872a975f0cde2f26425a724741be504b4be6116
Jason Cheng <yannru.cheng@gmail.com>
2014-09-18 14:18:52 -0400

    Added some comments to InventoryItem

--- MyZooPets/Assets/Scripts/Model/MutableData/InventoryItem.cs
+++ MyZooPets/Assets/Scripts/Model/MutableData/InventoryItem.cs
@@ -1,18 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//---------------------------------------------------
-// InventoryItem
-// Items that are stored in the inventory. 
-// Mutable data but contains reference to immutable data
-//---------------------------------------------------
+/// <summary>
+/// Items that are stored in the inventory. 
+/// Mutable data but contains reference to immutable data
+/// </summary>
 public class InventoryItem{
     public string ItemID {get; set;}
-    public string ItemTextureName {get; set;}
-    public ItemType ItemType {get; set;}
-    public int Amount {get; set;}
 
-    //Shortcut to return the immutable data of item with ItemID
+	/// <summary>
+	/// DEPRECATED .Gets or sets the name of the item texture. 
+	/// This data shouldn't be serialize since they are immutable.
+	/// Will be removed later.
+	/// </summary>
+	/// <value>The name of the item texture.</value>
+    public string ItemTextureName {get; set;}
+
+	/// <summary>
+	/// DEPRECATED .Gets or sets the type of the item. 
+	/// This data shouldn't be serialized since they are immutable.
+	/// Will be removed at later time.
+	/// </summary>
+	/// <value>The type of the item.</value>
+    public ItemType ItemType {get; set;}
+
+    public int Amount {get; set;}
+	
+	/// <summary>
+	/// Shortcut to return the immutable datda of item with ItemID
+	/// </summary>
+	/// <value>The item data.</value>
 	public Item ItemData{
 		get{return ItemLogic.Instance.GetItem(ItemID);}
 	}

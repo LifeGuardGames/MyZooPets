529859a53c99209dc4dd461aa0028fced9e6992f
Wenshiang Chung <aliken9@gmail.com>
2014-09-18 17:24:13 -0400

    Decoration overhaul Complete

--- MyZooPets/Assets/Scripts/View/InventoryUIManager.cs
+++ MyZooPets/Assets/Scripts/View/InventoryUIManager.cs
@@ -181,7 +181,6 @@ public class InventoryUIManager : Singleton<InventoryUIManager>{
 
 	//Event listener. listening to when new item is added to the inventory
 	private void OnItemAddedHandler(object sender, InventoryLogic.InventoryEventArgs e){
-		Debug.Log("ADDDED FOOOOOOD");
 		// inventory doesn't currently care about decorations/accessories
 		if(e.InvItem.ItemType == ItemType.Decorations || e.InvItem.ItemType == ItemType.Accessories)
 			return;
@@ -197,7 +196,6 @@ public class InventoryUIManager : Singleton<InventoryUIManager>{
 
 	//Create the NGUI object and populate the fields with InventoryItem data
 	private void SpawnInventoryItemInPanel(InventoryItem invItem, bool isOnLoad = false){
-		Debug.Log("SPawninventoryinPanel");
 		//Create inventory item
 		GameObject inventoryItemObject = NGUITools.AddChild(uiGridObject, inventoryItemPrefab);
 

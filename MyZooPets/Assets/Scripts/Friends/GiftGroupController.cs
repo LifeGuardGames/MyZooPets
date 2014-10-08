81dfdb986563a4987420b26cbbb85dcc0358f0ef
Wenshiang Chung <aliken9@gmail.com>
2014-10-08 12:36:44 -0400

    connection display fixes and gift group ready for integration

---
+++ MyZooPets/Assets/Scripts/Friends/GiftGroupController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiftGroupController : MonoBehaviour {
+	public UISprite gift1;
+	public UISprite gift2;
+	public UISprite gift3;
+	public UISprite gift4;
+	public ParticleSystem getGiftParticle;
+
+	private const string FullSpriteName = "friendsStarFull";
+	private const string EmptySpriteName = "friendsStarEmpty";
+
+	public void Initialize(){
+
+	}
+}

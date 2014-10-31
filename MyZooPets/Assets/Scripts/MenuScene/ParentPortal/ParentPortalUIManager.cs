f39d59f5b325528b8fb5f0945cd5598076d07608
Wenshiang Chung <aliken9@gmail.com>
2014-10-31 17:42:29 -0400

    new parent portal in

--- MyZooPets/Assets/Scripts/MenuScene/ParentPortal/ParentPortalUIManager.cs
+++ MyZooPets/Assets/Scripts/MenuScene/ParentPortal/ParentPortalUIManager.cs
@@ -6,71 +6,142 @@ using Parse;
 
 public class ParentPortalUIManager : SingletonUI<ParentPortalUIManager> {
 
-	public UIGrid grid;
-	public GameObject parentPortalEntryPetPrefab;
-	public GameObject parentPortalEntryCreatePrefab;
-	
+	public UILabel playerName;
+	public UILabel code;
+
+	public TweenToggle backgroundTween;
+
+	public TweenToggle mathTween;
+	public UILabel questionLabel;
+	public UILabel answerLabel;
+	private int answerToMath;
+	private int sequenceCount = 1;
+	private int answerSoFar;
+
+	public TweenToggle parentPortalTween;
 
 	void Awake(){
 		eModeType = UIModeTypes.ParentPortal;
 	}
 
 	void Start(){
-		ParentPortalManager.OnDataRefreshed += UpdateGrid;
+		// TODO JASON POPULATE HERE
+//		playerName.text = ;
+//		code.text = ;
 	}
 
-	void Destroy(){
-		ParentPortalManager.OnDataRefreshed -= UpdateGrid;
+	void OnDestroy(){
 	}
-	
-	private void UpdateGrid(object sender, ServerEventArgs args){
-		Debug.Log("updategrid");
-		foreach(Transform child in grid.transform){
-			child.gameObject.SetActive(false);
-			Destroy(child.gameObject);
-		}
 
-		//data refreshed successfully
-		if(args.IsSuccessful){
-			ParseObjectKidAccount account = ParentPortalManager.Instance.KidAccount;
-
-			GameObject portalEntryPet = NGUITools.AddChild(grid.gameObject, parentPortalEntryPetPrefab);
-
-			//set the code
-			UILabel codeLabel = portalEntryPet.FindInChildren("LabelCode").GetComponent<UILabel>();
-			codeLabel.text = account.AccountCode;
-
-			MutableDataPetMenuInfo menuInfo = DataManager.Instance.MenuSceneData;
-			UILabel nameLabel = portalEntryPet.FindInChildren("LabelName").GetComponent<UILabel>();
-			if(menuInfo != null)
-				nameLabel.text = menuInfo.PetName;
-
-			grid.Reposition();
-		}
-		else{
-			//display error message to user
-		}
-	}
 
 	protected override void _OpenUI(){
-		GetComponent<TweenToggleDemux>().Show();
+		OpenBackground();
+		OpenMathQuestion();
 
-		//Note: maybe do some cache check. don't need to do a server everytime
-		//the list opens
-		ParentPortalManager.Instance.RefreshData();
-
+		SelectionUIManager.Instance.gameObject.SetActive(false);
 	}
 
 	protected override void _CloseUI(){
-		GetComponent<TweenToggleDemux>().Hide();
+		CloseBackground();
+		CloseMathQuestion();
+		CloseParentPortal();
+
+		SelectionUIManager.Instance.gameObject.SetActive(true);
 	}
 
-		void OnGUI(){
-			if(GUI.Button(new Rect(100, 100, 100, 100), "Open")){
-				OpenUI();
-			}
-			if(GUI.Button(new Rect(200, 100, 100, 100), "Close")){
-				CloseUI();
-			}
+	private void OpenBackground(){
+		backgroundTween.Show();
+	}
+
+	private void CloseBackground(){
+		backgroundTween.Hide();
+	}
+
+	private void OpenMathQuestion(){
+		GenerateMathQuestion();
+		mathTween.Show();
+	}
+
+	private void CloseMathQuestion(){
+		mathTween.Hide();
+	}
+
+	private void OpenParentPortal(){
+		parentPortalTween.Show();
+	}
+
+	private void CloseParentPortal(){
+		parentPortalTween.Hide();
+	}
+
+	/// <summary>
+	/// Generates the math question.
+	/// NOTE: Answer will always be a 3 digit number
+	/// </summary>
+	private void GenerateMathQuestion(){
+		// Reset values
+		answerSoFar = 0;
+		sequenceCount = 1;
+		answerLabel.text = "___";
+
+		int firstNumber = UnityEngine.Random.Range(501, 999);
+		int secondNumber = UnityEngine.Random.Range(101, 499);
+		questionLabel.text = firstNumber.ToString() + " - " + secondNumber.ToString() + " = ";
+		answerToMath = firstNumber - secondNumber;
+	}
+
+	private void EnteredSequence(int digit){
+		switch(sequenceCount){
+		case 1:
+			answerSoFar = digit * 100;
+			answerLabel.text = digit.ToString() + "__";
+			break;
+		case 2:
+			answerSoFar += (digit * 10);
+			answerLabel.text = (answerSoFar / 10).ToString() + "_";
+			break;
+		case 3:
+			answerSoFar += digit;
+			answerLabel.text = answerSoFar.ToString();
+			break;
 		}
+		sequenceCount++;
+		if(sequenceCount >=4){
+			CheckAnswer();
+		}
+	}
+
+	private void CheckAnswer(){
+		if(answerSoFar == answerToMath){
+			// Success, go into parent portal
+			Debug.Log("Success!");
+			parentPortalTween.Show();
+			mathTween.Hide();
+		}
+		else{
+			// Fail, start over
+			Debug.Log("You're stupid!");
+			GenerateMathQuestion();
+		}
+	}
+
+	public void Button1(){ EnteredSequence(1); }
+	public void Button2(){ EnteredSequence(2); }
+	public void Button3(){ EnteredSequence(3); }
+	public void Button4(){ EnteredSequence(4); }
+	public void Button5(){ EnteredSequence(5); }
+	public void Button6(){ EnteredSequence(6); }
+	public void Button7(){ EnteredSequence(7); }
+	public void Button8(){ EnteredSequence(8); }
+	public void Button9(){ EnteredSequence(9); }
+	public void Button0(){ EnteredSequence(0); }
+
+	void OnGUI(){
+		if(GUI.Button(new Rect(100, 100, 100, 100), "Open")){
+			OpenUI();
+		}
+		if(GUI.Button(new Rect(200, 100, 100, 100), "Close")){
+			CloseUI();
+		}
+	}
 }

b070cce12fcd0dee9caa3914067c02365ae53ac8
aliken9 <aliken9@gmail.com>
2014-12-29 14:53:39 -0500

    New card memory card animations

--- MyZooPets/Assets/Scripts/MemoryGame/MemoryCard.cs
+++ MyZooPets/Assets/Scripts/MemoryGame/MemoryCard.cs
@@ -19,26 +19,42 @@ public class MemoryCard : MonoBehaviour {
 	public UISprite triggerSprite;
 	public UILabel triggerLabel;
 
+	public float activeTweenTime = 0.2f;
+	public float activeTweenScale = 1.1f;
+
+	private GameObject tweeningContentParent;
+	private GameObject tweeningCoverParent;
+
 	public void Initialize(ImmutableDataMemoryTrigger triggerData, bool isSprite){
 		triggerName = triggerData.Name;
 		this.isSprite = isSprite;
+
+		tweeningCoverParent = coverSprite.transform.parent.gameObject;
+
+		// Set components on start
 		if(isSprite){
 			triggerSprite.type = UISprite.Type.Simple;
 			triggerSprite.spriteName = triggerData.SpriteName;
+			tweeningContentParent = triggerSprite.transform.parent.gameObject;
+			triggerLabel.transform.parent.gameObject.SetActive(false);
 		}
 		else{
 			triggerLabel.text = triggerData.Name;
+			tweeningContentParent = triggerLabel.transform.parent.gameObject;
+			triggerSprite.transform.parent.gameObject.SetActive(false);
 		}
 
-		// Set components on start
-		coverSprite.gameObject.SetActive(true);
-		triggerSprite.gameObject.SetActive(false);
-		triggerLabel.gameObject.SetActive(false);
+		// Hide the original card content until its clicked
+		tweeningContentParent.SetActive(false);
 	}
 
 	void OnTap(TapGesture gesture){
 		CardFlipped();
 	}
+
+//	void OnFingerDown(FingerDownEvent e){ 
+//		Debug.Log("DSD");
+//	}
 
 	public void CardFlipped(){
 		// Check the manager to see if flip is allowed
@@ -47,30 +63,50 @@ public class MemoryCard : MonoBehaviour {
 			// Ping the memory game manager this was flipped
 			MemoryGameManager.Instance.NotifyClicked(this);
 
-			coverSprite.gameObject.SetActive(false);
-			if(isSprite){
-				triggerSprite.gameObject.SetActive(true);
-			}
-			else{
-				triggerLabel.gameObject.SetActive(true);
-			}
+			PlayFlipSquenceOpen();
 		}
 	}
 
+	private void PlayFlipSquenceOpen(){
+		// Zoom in on the whole object
+		LeanTween.scale(gameObject, new Vector3(activeTweenScale, activeTweenScale, 1), activeTweenTime);
+		
+		LeanTween.scaleX(tweeningCoverParent, 0f, activeTweenTime / 2f).setOnComplete(FlipSequenceOpenHelper);
+	}
+	
+	private void FlipSequenceOpenHelper(){
+		tweeningCoverParent.SetActive(false);
+
+		tweeningContentParent.transform.localScale = new Vector3(0f, 1f, 1f);
+		tweeningContentParent.SetActive(true);
+		LeanTween.scaleX(tweeningContentParent, 1f, activeTweenTime / 2f);
+	}
+
+	private void PlayFlipSequenceClose(){
+		// Zoom back out on the whole object
+		LeanTween.scale(gameObject, new Vector3(1, 1, 1), activeTweenTime);
+		
+		LeanTween.scaleX(tweeningContentParent, 0f, activeTweenTime / 2f).setOnComplete(FlipSequenceCloseHelper);
+	}
+	
+	private void FlipSequenceCloseHelper(){
+		tweeningContentParent.SetActive(false);
+
+		tweeningCoverParent.transform.localScale = new Vector3(0f, 1f, 1f);
+		tweeningCoverParent.SetActive(true);
+		LeanTween.scaleX(tweeningCoverParent, 1f, activeTweenTime / 2f);
+	}
+
+	/// <summary>
+	/// External call from memory game manager
+	/// </summary>
+	/// <param name="isSuccess">If set to <c>true</c> is success.</param>
 	public void FlipResult(bool isSuccess){
 		if(isSuccess){
 			Destroy(gameObject);
 		}
 		else{
-			// Flip back the card and do nothing
-			coverSprite.gameObject.SetActive(true);
-
-			if(isSprite){
-				triggerSprite.gameObject.SetActive(false);
-			}
-			else{
-				triggerLabel.gameObject.SetActive(false);
-			}
+			PlayFlipSequenceClose();
 		}
 	}
 }

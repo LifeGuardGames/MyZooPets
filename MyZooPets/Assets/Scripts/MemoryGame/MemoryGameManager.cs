7531b18665550c39f07a6f38540541e197ffdc70
Wenshiang Chung <aliken9@gmail.com>
2015-01-05 18:06:01 -0500

    Art changes to memory game, pause support in memorymanager

--- MyZooPets/Assets/Scripts/MemoryGame/MemoryGameManager.cs
+++ MyZooPets/Assets/Scripts/MemoryGame/MemoryGameManager.cs
@@ -21,6 +21,7 @@ public class MemoryGameManager : MinigameManager<MemoryGameManager> {
 	private bool pauseDelayActive = false;
 	private float cardDelayTimer = 0.8f;
 	private int combo = 0;
+	private bool isPaused = false;
 
 	void Awake(){
 		quitGameScene = SceneUtils.BEDROOM;
@@ -28,10 +29,11 @@ public class MemoryGameManager : MinigameManager<MemoryGameManager> {
 
 	#region Overridden Functions
 	protected override void _Start(){
+		MemoryGameManager.OnStateChanged += GameStateChange;
 	}
 
 	protected override void _OnDestroy(){
-
+		MemoryGameManager.OnStateChanged -= GameStateChange;
 	}
 
 	protected override string GetMinigameKey(){
@@ -66,6 +68,21 @@ public class MemoryGameManager : MinigameManager<MemoryGameManager> {
 		// TODO add badges
 	}
 
+	private void GameStateChange(object sender, GameStateArgs args){
+		switch(args.GetGameState()){
+		case MinigameStates.GameOver:
+			break;
+		case MinigameStates.Paused:
+			Debug.Log("Paused");
+			isPaused = true;
+			break;
+		case MinigameStates.Playing:
+			Debug.Log("Playing");
+			isPaused = false;
+			break;
+		}
+	}
+
 	public override int GetReward(MinigameRewardTypes eType){
 		return GetStandardReward(eType);
 	}
@@ -77,12 +94,14 @@ public class MemoryGameManager : MinigameManager<MemoryGameManager> {
 	/// InvokeRepeating method from _NewGame()
 	/// </summary>
 	private void StartScoreCountdown(){
-		// Check for negative score
-		if(GetScore() - scoreDecrementValue >= 0){
-			UpdateScore(scoreDecrementValue * -1);
-		}
-		else{
-			SetScore(0);
+		if(!isPaused){
+			// Check for negative score
+			if(GetScore() - scoreDecrementValue >= 0){
+				UpdateScore(scoreDecrementValue * -1);
+			}
+			else{
+				SetScore(0);
+			}
 		}
 	}
 
@@ -171,10 +190,10 @@ public class MemoryGameManager : MinigameManager<MemoryGameManager> {
 		flip2 = null;
 	}
 
-//	void OnGUI(){
-//		if(GUI.Button(new Rect(100, 100, 100, 100), "test")){
-//			ResetBoard();
-//		}
-//	}
+	void OnGUI(){
+		if(GUI.Button(new Rect(100, 100, 100, 100), "test")){
+			GameOver();
+		}
+	}
 	#endregion
 }

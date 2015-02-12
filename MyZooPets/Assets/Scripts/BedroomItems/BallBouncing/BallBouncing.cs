3797b81a2e113974c6a1698e6d7da81e3b48f032
Dylan <dylan@lifeguardgames.com>
2015-02-12 18:40:19 -0500

    added ball bouncing currently stored in a prefab called ball bouncing

--- MyZooPets/Assets/Scripts/BedroomItems/BallBouncing/BallBouncing.cs
+++ MyZooPets/Assets/Scripts/BedroomItems/BallBouncing/BallBouncing.cs
@@ -2,30 +2,15 @@
 using System.Collections;
 
 public class BallBouncing : MonoBehaviour {
-
-	public Vector3 cameraZoom;
-	public int combo;
-	public GameObject zoomItem;
-	public Vector3 zoomRotation;
-	public float zoomTime;
+	
 	public GameObject backButton;
 	public GameObject comboLabel;
 	public bool isActive = false;
 	public Vector2 force;
-
-	// Use this for initialization
-	void Start () {
+	public int combo;
+	public GameObject floor;
 	
-	}
 	void openBallGame(){
-		/*if(!isActive){
-			// Zoom into the item
-			Vector3 targetPosition = zoomItem.transform.position + cameraZoom;
-			
-			CameraManager.Callback cameraDoneFunction = delegate(){
-			};
-			CameraManager.Instance.ZoomToTarget(targetPosition, zoomRotation, zoomTime, cameraDoneFunction);
-			MovePet();
 			// Hide other UI objects
 			NavigationUIManager.Instance.HidePanel();
 			InventoryUIManager.Instance.HidePanel();
@@ -33,11 +18,8 @@ public class BallBouncing : MonoBehaviour {
 			
 			//need to disable more things here
 			PetAnimationManager.Instance.DisableIdleAnimation();
-			isActive = true;
 			backButton.SetActive(true);
 			PetMovement.Instance.canMove = false;
-		}*/
-	
 	}
 
 	/// <summary>
@@ -49,16 +31,18 @@ public class BallBouncing : MonoBehaviour {
 		if(!pet.renderer.isVisible)
 			PetMovement.Instance.petSprite.transform.position = new Vector3(transform.position.x-4f, 0, 26.65529f);
 		PetMovement.Instance.MovePet(this.gameObject.transform.position);
-		
 	}
 
 	void OnTap (TapGesture e){
-		if(!isActive){
-			this.rigidbody.useGravity = true;
-		}
-		else{
+		Debug.Log("working");
 		combo++;
-		transform.rigidbody2D.AddForceAtPosition(force, e.Position);
+		Debug.Log(new Vector2 ((transform.position.x - e.Position.x),force.y));
+		transform.rigidbody.AddForceAtPosition(new Vector2 (((transform.position.x - e.Position.x)+300),force.y), e.Position);
+	}
+
+	void OnCollisionEnter (Collision col){
+		if(col.gameObject ==  floor){
+			combo = 0;
 		}
 	}
 

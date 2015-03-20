f5f7cbcbc02de51147492461a6bd78e48dd8d0b6
aliken9 <aliken9@gmail.com>
2015-03-20 15:59:04 -0400

    Failed attempt at hatching pet, pushing scripts for another time

---
+++ MyZooPets/Assets/Scripts/View/ParticleColorChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleColorChange : MonoBehaviour{
+
+	public ParticleSystem particle;
+	public string color1Name;
+	public Color color1;
+
+	public string color2Name;
+	public Color color2;
+
+	public string color3Name;
+	public Color color3;
+
+	public string color4Name;
+	public Color color4;
+
+	public void ChangeColor(string colorName){
+		if(string.Equals(colorName, color1Name)){
+			particle.startColor = color1;
+		}
+		else if(string.Equals(colorName, color2Name)){
+			particle.startColor = color2;
+		}
+		else if(string.Equals(colorName, color3Name)){
+			particle.startColor = color3;
+		}
+		else if(string.Equals(colorName, color4Name)){
+			particle.startColor = color4;
+		}
+		else{
+			Debug.LogWarning("No valid colors detected!");
+		}
+		particle.Stop();
+	}
+}

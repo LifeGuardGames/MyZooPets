0349bda979651928c955cd6f45f3347f127da738
Wenshiang Chung <aliken9@gmail.com>
2015-04-01 18:12:17 -0400

    High score integration WIP

--- MyZooPets/Assets/Scripts/Highscore/HighScoreManager.cs
+++ MyZooPets/Assets/Scripts/Highscore/HighScoreManager.cs
@@ -13,14 +13,4 @@ public class HighScoreManager : Singleton<HighScoreManager> {
     public void UpdateMinigameHighScore(string miniGameKey, int highScore){
         DataManager.Instance.GameData.HighScore.UpdateMinigameHighScore(miniGameKey, highScore);
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }

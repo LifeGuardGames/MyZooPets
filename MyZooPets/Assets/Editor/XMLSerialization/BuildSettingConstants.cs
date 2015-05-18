98c21ca918eb156f4e3603c7b1a0b1a817073b6e
Wenshiang Chung <aliken9@gmail.com>
2015-05-18 13:56:23 -0400

    Fixing lg debug settings

--- MyZooPets/Assets/Editor/XMLSerialization/BuildSettingConstants.cs
+++ MyZooPets/Assets/Editor/XMLSerialization/BuildSettingConstants.cs
@@ -6,13 +6,4 @@ using System.Collections.Generic;
 public class BuildSettingConstants{
     [XmlElement("Constant")]
     public List<Constant> BuildSettingConstantList = new List<Constant>();
-    // [XmlElement("Constant")]
-    // public Constant LiteBundleID{get; set;}
-
-    // [XmlElement("Constant")]
-    // public Constant ProBundleID{get; set;}
-    // // [XmlElement("Constant")]
-    // public Constant IsLiteVersion{get; set;}
-    // // [XmlElement("Constant")]
-    // public Constant AnalyticsEnabled{get; set;}
 }

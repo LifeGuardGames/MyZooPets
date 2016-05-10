650fd78d96122092f5e3cf87907849218371df30
aliken9 <aliken9@gmail.com>
2016-05-10 11:49:06 -0400

    Updated GameObjectUtils and NewPrototype for DoctorMatch

--- MyZooPets/Assets/Scripts/DoctorMatch/AssemblyLineController.cs
+++ MyZooPets/Assets/Scripts/DoctorMatch/AssemblyLineController.cs
@@ -3,13 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AssemblyLineController : MonoBehaviour {
+	public List<Transform> positionList;
+	public GameObject itemParent;
 	public GameObject itemPrefab;
 
-	void Start(){
+	private Queue<AssemblyLineItem> itemQueue;
 
+	// Fill the list with items and in the right positions
+	public void Initialize(){
+		if(positionList == null || positionList.Count == 0){
+			Debug.LogError("Position list not initialized or empty");
+			return;
+		}
+
+		itemQueue = new Queue<AssemblyLineItem>();
+
+		// Init and throw it into the queue
+		for(int i = 0; i < positionList.Count; i++){
+			GameObject item = GameObjectUtils.AddChild(itemParent, itemPrefab);
+			item.transform.position = positionList[i].position;
+			AssemblyLineItem itemScript = item.GetComponent<AssemblyLineItem>();
+			itemScript.Init(i);
+			itemQueue.Enqueue(itemScript);
+		}
 	}
 
-	public void PopAndFill(){
+	public AssemblyLineItem PopFirstItem(){
+		AssemblyLineItem poppedItem = itemQueue.Dequeue();
+		return poppedItem;
+	}
 
+	public void ShiftAndAddNewItem(){
+		foreach(AssemblyLineItem itemScript in itemQueue){
+			int newIndex = itemScript.GetIncrementIndex();
+			LeanTween.cancel(itemScript.gameObject);
+			LeanTween.move(itemScript.gameObject, positionList[newIndex].position, 0.1f);
+		}
+
+		// Add new item
+		GameObject item = GameObjectUtils.AddChild(itemParent, itemPrefab);
+		int newItemIndex = positionList.Count - 1;
+		item.transform.position = positionList[newItemIndex].position;
+		AssemblyLineItem newItemScript = item.GetComponent<AssemblyLineItem>();
+		newItemScript.Init(newItemIndex);
+		itemQueue.Enqueue(newItemScript);
+	}
+
+	public void DestroyItems(){
+		foreach (Transform child in itemParent.transform) {
+			GameObject.Destroy(child.gameObject);
+		}
 	}
 }
--- MyZooPets/Assets/Scripts/DoctorMatch/AssemblyLineItem.cs
+++ MyZooPets/Assets/Scripts/DoctorMatch/AssemblyLineItem.cs
@@ -2,5 +2,60 @@
 using System.Collections;
 
 public class AssemblyLineItem : MonoBehaviour {
+	public SpriteRenderer itemSprite;
 
+	private DoctorMatchManager.DoctorMatchButtonTypes itemType;
+	public DoctorMatchManager.DoctorMatchButtonTypes ItemType{
+		get{
+			return itemType;
+		}
+	}
+
+	private int index;
+
+	public void Init(int currentIndex){
+		index = currentIndex;
+
+		// Generate random type and populate everything
+		int randomIndex = UnityEngine.Random.Range(0, 3);
+		if(randomIndex == 1){
+			itemType = DoctorMatchManager.DoctorMatchButtonTypes.Green;
+		}
+		else if(randomIndex == 2){
+			itemType = DoctorMatchManager.DoctorMatchButtonTypes.Yellow;
+		}
+		else{
+			itemType = DoctorMatchManager.DoctorMatchButtonTypes.Red;
+		}
+		itemSprite.sprite = LoadSpriteZoneType(itemType);
+	}
+
+	public void Activate(){
+		Destroy(gameObject, 0.05f);	
+	}
+
+	public int GetIncrementIndex(){
+		index--;
+		return index;
+	}
+
+	private Sprite LoadSpriteZoneType(DoctorMatchManager.DoctorMatchButtonTypes type){
+		int randomIndex = UnityEngine.Random.Range(1, 6);
+		Sprite spriteData = null;
+		switch(type){
+		case DoctorMatchManager.DoctorMatchButtonTypes.Green:
+			spriteData = Resources.Load<Sprite>("happy_" + randomIndex);
+			break;
+		case DoctorMatchManager.DoctorMatchButtonTypes.Yellow:
+			spriteData = Resources.Load<Sprite>("sick1_" + randomIndex);
+			break;
+		case DoctorMatchManager.DoctorMatchButtonTypes.Red:
+			spriteData = Resources.Load<Sprite>("sick2_" + randomIndex);
+			break;
+		default:
+			Debug.LogError("Invalid type " + type.ToString());
+			break;
+		}
+		return spriteData;
+	}
 }

3b65e121ad7081bae6abae82d9e6f95ae1ece617
aliken9 <aliken9@gmail.com>
2016-07-19 19:05:09 -0400

    Start fixing MenuScene

--- MyZooPets/Assets/Scripts/MenuScene/CustomizationUIManager.cs
+++ MyZooPets/Assets/Scripts/MenuScene/CustomizationUIManager.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class CustomizationUIManager:Singleton<CustomizationUIManager>{
-	public TweenToggle colorTweenParent;	// Part 1 of the selection process
-	public TweenToggle nameTweenParent;		// Part 2 of the selection process
-    public Text nameField;
+public class CustomizationUIManager : Singleton<CustomizationUIManager> {
+	public TweenToggle colorTweenParent;    // Part 1 of the selection process
+	public TweenToggle nameTweenParent;     // Part 2 of the selection process
+	public Text nameField;
 	public ParticleSystemController leafParticle;
 	public Animation requireNameAnimation;
 	public Animation requireColorAnimation;
@@ -15,30 +12,29 @@ public class CustomizationUIManager:Singleton<CustomizationUIManager>{
 	public TweenToggle logoTitleTween;
 	public bool canClickHatchPet = false;
 
-    private string petColor = null;
-    private string petName; //Default pet name
-    private Color currentRenderColor;
+	private string petColor = null;
+	private string petName; //Default pet name
+	private Color currentRenderColor;
 
 	private int hatchClicksCount = 7;
 
-	
-	public void  _OpenUI(){
+	public void _OpenUI() {
 		logoTitleTween.Hide();
-        ShowFirstChooseUI();
+		ShowFirstChooseUI();
 	}
-	
+
 	// Used when pressing back button in the panel
-	public void _CloseUI(){
+	public void _CloseUI() {
 		nameTweenParent.Hide();
 	}
-    
-    public void ChangeEggColor(string petColor){
-		poofParticle.Play();
-		EggController.Instance.ChangeColor("egg"+petColor);
-		this.petColor = petColor;     
-    }
 
-	private void ShowFirstChooseUI(){
+	public void ChangeEggColor(string petColor) {
+		poofParticle.Play();
+		EggController.Instance.ChangeColor("egg" + petColor);
+		this.petColor = petColor;
+	}
+
+	private void ShowFirstChooseUI() {
 		EggController.Instance.ToggleEggIdleAnimation(false);
 		EggController.Instance.EggCrack(1);
 		leafParticle.Stop();
@@ -46,12 +42,12 @@ public class CustomizationUIManager:Singleton<CustomizationUIManager>{
 		colorTweenParent.Show();
 	}
 
-	public void FirstFinishClicked(){
-		if(petColor != null){
+	public void FirstFinishClicked() {
+		if(petColor != null) {
 			colorTweenParent.Hide();
 			EggController.Instance.EggCrack(2);
 		}
-		else{
+		else {
 			requireColorAnimation.Play();
 		}
 	}
@@ -59,22 +55,21 @@ public class CustomizationUIManager:Singleton<CustomizationUIManager>{
 	/// <summary>
 	/// Shows the second choose UI, called from first finish callback
 	/// </summary>
-	public void ShowSecondChooseUI(){
+	public void ShowSecondChooseUI() {
 		nameTweenParent.Show();
 	}
 
-	public void SecondFinishClicked(){
-		if(!String.IsNullOrEmpty(nameField.text)){
+	public void SecondFinishClicked() {
+		if(!string.IsNullOrEmpty(nameField.text)) {
 			petName = nameField.text;
 			_CloseUI();
 
 			Analytics.Instance.PetColorChosen(this.petColor);
-
 			
 			// Initialize data for new pet
-			DataManager.Instance.ModifyBasicPetInfo(petName:petName, petSpecies:"Basic", petColor:this.petColor);
+			DataManager.Instance.ModifyBasicPetInfo(petName: petName, petSpecies: "Basic", petColor: this.petColor);
 		}
-		else{
+		else {
 			// Play the animation to prompt user to enter name
 			requireNameAnimation.Play();
 		}
@@ -83,17 +78,17 @@ public class CustomizationUIManager:Singleton<CustomizationUIManager>{
 	/// <summary>
 	/// Shows the third choose UI, called from second finish callback
 	/// </summary>
-	public void ShowThirdChooseUI(){
+	public void ShowThirdChooseUI() {
 		canClickHatchPet = true;
 
 		// Show the finger pointer here
 		EggController.Instance.ToggleFingerHint(true);
 	}
 
-	public void ThirdUIEggTapped(){
+	public void ThirdUIEggTapped() {
 		EggController.Instance.EggHatchingTapped();
 		hatchClicksCount--;
-		if(hatchClicksCount <= 0){	// Hatch the pet
+		if(hatchClicksCount <= 0) { // Hatch the pet
 			SelectionUIManager.instance.PlayMovie(petColor);
 		}
 	}
--- MyZooPets/Assets/Scripts/View/GuiHelpers/ButtonSetHighlight.cs
+++ MyZooPets/Assets/Scripts/View/GuiHelpers/ButtonSetHighlight.cs
@@ -1,39 +1,23 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonSetHighlight : MonoBehaviour {
-	
-	public LgButton[] buttonList;	// List of locations that needs to move
-	public LgButton firstButton;	// Starting location
-	
+	public RectTransform firstButtonTrans;	// Starting location
+
 	void Start(){
-		if(firstButton != null){
-			gameObject.transform.position = firstButton.transform.position;
+		if(firstButtonTrans != null){
+			gameObject.GetComponent<RectTransform>().sizeDelta = firstButtonTrans.sizeDelta;
 		}
 		else{
-			GetComponent<UISprite>().enabled = false;
+			GetComponent<Image>().enabled = false;
 		}
-		
-		foreach(LgButton button in buttonList){
-			button.OnProcessed += ChangePosition;
-		}
-
 		_Start();
 	}
 
 	// Override this in child, used for conditional position changes
 	protected virtual void _Start(){}
-
-	public void SetFirstButton(){
-		if(firstButton != null){
-			gameObject.transform.position = firstButton.transform.position;
-		}
-	}
 	
-	private void ChangePosition(object sender, EventArgs args){
-		GetComponent<UISprite>().enabled = true;
-		LgButton button = (LgButton)sender;
-		gameObject.transform.position = button.transform.position;
+	public void ChangePosition(Transform trans){
+		gameObject.transform.position = trans.position;
 	}
 }

bcc65f41186b7d69a511734a4e7f0c395018c037
aliken9 <aliken9@gmail.com>
2016-07-19 16:14:44 -0400

    SecretPanelUI prefab made

---
+++ MyZooPets/Assets/Scripts/SecretUIController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretUIController : Singleton<MonoBehaviour> {
+
+	public TweenToggle tween;
+
+	public void ShowPanel(){
+		tween.Show();
+	}
+
+	public void HidePanel(){
+		tween.Hide();
+	}
+}

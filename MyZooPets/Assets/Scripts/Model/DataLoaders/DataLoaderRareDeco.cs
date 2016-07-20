6a0038c6537b09355486422b40068962eb71587b
Dylan <dylan@lifeguardgames.com>
2016-07-20 15:03:43 -0400

    Rare deco machine WIP

---
+++ MyZooPets/Assets/Scripts/Model/DataLoaders/DataLoaderRareDeco.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataLoaderRareDeco : XMLLoaderGeneric<DataLoaderRareDeco> {
+
+
+	public static ImmutableDataRareDeco GetItem(string id) {
+		instance.InitXMLLoader();
+
+		return instance.GetData<ImmutableDataRareDeco>(id);
+
+	}
+
+	public static List<ImmutableDataRareDeco> GetItemList() {
+		instance.InitXMLLoader();
+		return instance.GetDataList<ImmutableDataRareDeco>();
+	}
+
+	public static ImmutableDataRareDeco GetDecoAtTier(int tier) {
+		List<ImmutableDataRareDeco> temp = GetItemList();
+		foreach(ImmutableDataRareDeco deco in temp) {
+			if(deco.Tier > tier) {
+				temp.Remove(deco);
+			}
+		}
+		int rand = UnityEngine.Random.Range(0, temp.Count);
+		return temp[rand];
+	}
+
+
+	protected override void XMLNodeHandler(string id, IXMLNode xmlNode, Hashtable hashData, string errorMessage) {
+		ImmutableDataRareDeco data = new ImmutableDataRareDeco(id, ItemType.Decorations, hashData);
+
+		// store the data
+		if(hashData.ContainsKey(id))
+			Debug.LogError(errorMessage + "Duplicate keys!");
+		else
+			hashData.Add(id, data);
+	}
+
+	protected override void InitXMLLoader() {
+		xmlFileFolderPath = "RareDeco";
+	}
+}
---
+++ MyZooPets/Assets/Scripts/RareDecoMachine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RareDecoMachine : MonoBehaviour {
+
+	public int level;
+	public PositionTweenToggle openingScreen;
+	public PositionTweenToggle outPutScreen;
+	public PositionTweenToggle levelUpScreen;
+
+	void Start() {
+		level = DataManager.Instance.GameData.Decorations.capsuleMachineLevel;
+    }
+
+	public void OnUse() {
+		openingScreen.Show();
+	}
+
+	public void RollOnDeco() {
+		openingScreen.Hide();
+		ImmutableDataRareDeco capsule = DataLoaderRareDeco.GetDecoAtTier(level);
+		InventoryManager.Instance.AddItemToInventory(capsule.ID);
+	}
+
+	public void Upgrade() {
+		level++;
+		DataManager.Instance.GameData.Decorations.capsuleMachineLevel++;
+	}
+}

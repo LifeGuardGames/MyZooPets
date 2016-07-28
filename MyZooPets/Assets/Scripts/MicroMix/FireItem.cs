53445ba7fe09a76d1b5401e03c28c336c56d2d32
Benjamin Sisson <benmsisson@gmail.com>
2016-07-28 18:21:16 -0400

    Finished fire Micro. May require some switching and visual movement

--- MyZooPets/Assets/Scripts/MicroMix/FireItem.cs
+++ MyZooPets/Assets/Scripts/MicroMix/FireItem.cs
@@ -2,42 +2,52 @@
 using System.Collections;
 
 public class FireItem : MicroItem{
-	public GameObject mover;
+	public Sprite fireOff;
+	public Sprite fireOn;
 	private bool complete;
-	private bool holding = false;
 	private bool started = false;
-
+	private float shrinkSize = .95f;
+	private float shrinkTime = .1f;
+	// Use this for initialization
 	public override void StartItem(){
 		complete = false;
 		started = false;
+		GetComponent<SpriteRenderer>().sprite = fireOff;
 	}
 
 	public override void OnComplete(){
-		
+
+	}
+
+	public void Engage(){
+		GetComponent<SpriteRenderer>().sprite = fireOn;
+		LeanTween.scale(gameObject, new Vector3(shrinkSize, shrinkSize), shrinkTime).setEase(LeanTweenType.easeInOutQuad);
+		FireMicro fm = GetComponentInParent<FireMicro>(); //Called when we have not been set up in tutorial so we just use this instead
+		fm.StartBar();
+	}
+
+	public void Disengage(){
+		GetComponent<SpriteRenderer>().sprite = fireOff;
+		LeanTween.scale(gameObject, new Vector3(1, 1), shrinkTime).setEase(LeanTweenType.easeInOutQuad);
+		FireMicro fm = GetComponentInParent<FireMicro>();
+		fm.StopBar();
 	}
 
 	void OnMouseDown(){
-		if(!complete){
-			FireMicro fm = (FireMicro)parent;
-			fm.StartMover();
+		if(!complete && !MicroMixManager.Instance.IsPaused && !MicroMixManager.Instance.IsTutorial){
 			complete = true;
 			started = true;
-
+			Engage();
 		}
 	}
 
 	void OnMouseUp(){
-		if(started){
+		if(started && !MicroMixManager.Instance.IsPaused && !MicroMixManager.Instance.IsTutorial){
+			Disengage();
 			FireMicro fm = (FireMicro)parent;
-			fm.StopMover();
 			started = false;
-			parent.SetWon(Correct());
-			Debug.Log(Correct());
+			parent.SetWon(fm.IsCorrect());
 		}
 	}
-
-	private bool Correct(){
-		float y = mover.transform.position.y;
-		return Mathf.Abs(y) < .3f;
-	}
+		
 }
--- MyZooPets/Assets/Scripts/MicroMix/FireMicro.cs
+++ MyZooPets/Assets/Scripts/MicroMix/FireMicro.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 
 public class FireMicro : Micro{
-	public GameObject mover;
+	public GameObject barUI;
+	public RectTransform innerBar;
 	private float moveTime = .8f;
+	private float minSize = -345f;
+	private float maxSize = -4.3f;
 	private LeanTweenType type;
 	// Use this for initialization
 	public override int Background{
@@ -18,51 +21,71 @@ public class FireMicro : Micro{
 		}
 	}
 
-	public void StartMover(){
+	public void StartBar(){
 		TweenUp();
 	}
 
-	public void StopMover(){
-		LeanTween.cancel(mover);
+	public void StopBar(){
+		LeanTween.cancel(innerBar.gameObject);
+	}
+
+	public bool IsCorrect(){ //If you want to change the range, then make sure to change the UI elements too.
+		return innerBar.offsetMax.x > -85f;
 	}
 
 	protected override void _StartMicro(int difficulty, bool randomize){
-		type = LeanTweenType.easeInOutSine;
+		if(randomize){
+			Setup();
+		}
 	}
 
 	protected override void _EndMicro(){
+		StopBar();
+		barUI.SetActive(false);
 	}
 
 	protected override IEnumerator _Tutorial(){
-		Vector3 moverPos = mover.transform.position;
+		Setup();
 		MicroMixFinger finger = MicroMixManager.Instance.finger;
 		finger.gameObject.SetActive(true);
-		Vector3 button = GetComponentInChildren<FireItem>().gameObject.transform.position;
+		FireItem fireItem = GetComponentInChildren<FireItem>();
+		Vector3 button = fireItem.gameObject.transform.position;
 		Vector3 offset = new Vector3(0, 1);
 		yield return finger.MoveTo(button + offset, button, delay: .3f, time: .3f);
 
-		TweenUp();
-		yield return WaitSecondsPause(1.08f);
-		LeanTween.cancel(mover);
+		fireItem.Engage();
+		yield return WaitSecondsPause(.72f);
+		fireItem.Disengage();
 
 		yield return finger.MoveTo(button, button + offset, delay: 0, time: .3f);
 		finger.gameObject.SetActive(false);
-		mover.transform.position = moverPos;
+
+		UpdateSize(minSize);
 	}
 
 	protected override void _Pause(){
-		LeanTween.pause(mover);
+		LeanTween.pause(innerBar.gameObject);
 	}
 
 	protected override void _Resume(){
-		LeanTween.resume(mover);
+		LeanTween.resume(innerBar.gameObject);
+	}
+
+	private void Setup(){
+		type = LeanTweenType.linear;
+		UpdateSize(minSize);
+		barUI.SetActive(true);
 	}
 
 	private void TweenUp(){
-		LeanTween.move(mover, new Vector3(0, 2.45f), moveTime).setEase(type).setOnComplete(TweenBack);
+		LeanTween.value(innerBar.gameObject, minSize, maxSize, moveTime).setEase(type).setOnUpdate(UpdateSize).setOnComplete(TweenBack);
 	}
 
 	private void TweenBack(){
-		LeanTween.move(mover, new Vector3(0, -2.45f), moveTime).setEase(type).setOnComplete(TweenUp);
+		LeanTween.value(innerBar.gameObject, maxSize, minSize, moveTime).setEase(type).setOnUpdate(UpdateSize).setOnComplete(TweenUp);
+	}
+
+	private void UpdateSize(float newSize){
+		innerBar.offsetMax = new Vector2(newSize, innerBar.offsetMax.y);
 	}
 }

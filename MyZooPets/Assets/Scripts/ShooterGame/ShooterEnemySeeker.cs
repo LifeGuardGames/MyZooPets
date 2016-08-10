b5a2390f4f5c69f0400f69c3ea04f34220ad6211
Dylan <dylan@lifeguardgames.com>
2016-08-10 13:38:32 -0400

    pushing for legacy merge

--- MyZooPets/Assets/Scripts/ShooterGame/ShooterEnemySeeker.cs
+++ MyZooPets/Assets/Scripts/ShooterGame/ShooterEnemySeeker.cs
@@ -10,6 +10,12 @@ public class ShooterEnemySeeker : ShooterEnemy {
 
 	IEnumerator FindingTarget() {
 		yield return new WaitForSeconds(2.0f);
-		LeanTween.move(this.gameObject, player.transform.position, moveDuration);
+		LeanTween.move(this.gameObject, new Vector3 (player.transform.position.x- (player.transform.position.x - 100), player.transform.position.y-(player.transform.position.y - 100), player.transform.position.z), 1.0f);
+		StartCoroutine("Seeking");
 	}
+	IEnumerator Seeking() {
+		yield return new WaitForSeconds(3.0f);
+		LeanTween.move(this.gameObject, player.transform.position, moveDuration);
+    }
+
 }

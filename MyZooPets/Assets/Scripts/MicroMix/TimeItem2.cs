e0851ba43c3b569b10db9ed1d50a06b1dd1a7dc7
Benjamin Sisson <benmsisson@gmail.com>
2016-08-19 13:30:54 -0400

    Updates to TimeMicro

---
+++ MyZooPets/Assets/Scripts/MicroMix/TimeItem2.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeItem2 : MicroItem{
+	public GameObject sun;
+	public GameObject clock1;
+	public GameObject clock2;
+	private float validDist = 1f;
+	private bool hitOne = false;
+	private bool pressing = false;
+	private float size = 2f;
+	private float shrinkTime = .15f;
+	//If we got the first correct
+	public override void StartItem(){
+		clock1.SetActive(true);
+		clock2.SetActive(true);
+		hitOne = false;
+		pressing = false;
+		gameObject.transform.localScale = new Vector3(size, size, 1f);
+	}
+
+	public override void OnComplete(){
+		LeanTween.cancel(gameObject);
+	}
+
+	public void ShrinkDown(){
+		LeanTween.scale(gameObject, new Vector3(size - .2f, size - .2f), shrinkTime).setEase(LeanTweenType.easeInOutQuad).setOnComplete(GrowBack);
+	}
+
+	void OnTap(TapGesture gesture){
+		if(gesture.Selection != gameObject || MicroMixManager.Instance.IsPaused || MicroMixManager.Instance.IsTutorial || pressing){
+			return;
+		}
+		ShrinkDown();
+		pressing = true;
+		if(Vector3.SqrMagnitude(clock1.transform.position - sun.transform.position) < validDist){
+			clock1.SetActive(false);
+			hitOne = true;
+		}
+		if(Vector3.SqrMagnitude(clock2.transform.position - sun.transform.position) < validDist){
+			clock2.SetActive(false);
+			if(hitOne){
+				parent.SetWon(true);
+			}
+		}
+	}
+
+	private void GrowBack(){
+		LeanTween.scale(gameObject, new Vector3(size, size), shrinkTime).setEase(LeanTweenType.easeInOutQuad).setOnComplete(FinishPress);
+	}
+
+	private void FinishPress(){
+		pressing = false;
+	}
+}
--- MyZooPets/Assets/Scripts/MicroMix/TimeMicro2.cs
+++ MyZooPets/Assets/Scripts/MicroMix/TimeMicro2.cs
@@ -2,10 +2,9 @@
 using System.Collections;
 
 public class TimeMicro2 : Micro{
-	public GameObject clock1;
-	public GameObject clock2;
 	public GameObject sunParent;
 	public GameObject moon;
+	public GameObject sun;
 	public GameObject background;
 	private Vector3 moonHigh = new Vector3(0, 1.5f);
 	private Vector3 moonLow = new Vector3(0, -4f);
@@ -13,6 +12,9 @@ public class TimeMicro2 : Micro{
 	private float transitionLength = .6f;
 	//Must be set according to Glinda's anim
 	private bool isDay;
+	private bool paused = false;
+	private float angle;
+	//Used for tutorial
 
 	public override string Title{
 		get{
@@ -27,7 +29,7 @@ public class TimeMicro2 : Micro{
 	}
 
 	void Update(){
-		if (MicroMixManager.Instance.IsPaused){
+		if(MicroMixManager.Instance.IsPaused || paused){
 			return;
 		}
 		time += Time.deltaTime;
@@ -39,48 +41,88 @@ public class TimeMicro2 : Micro{
 			isDay = false;
 			DayToNight();
 		}
-		float angle = -time/4f*360;
-		sunParent.transform.rotation= Quaternion.Euler(new Vector3(0,0,angle));
+		angle -= 270 / 4f * Time.deltaTime;
+		sunParent.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 	}
 
 	protected override void _StartMicro(int difficulty, bool randomize){
 		time = 0;
-		clock1.SetActive(true);
-		clock2.SetActive(true);
+		sunParent.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		sun.transform.localPosition = Vector3.down * 5;
 		isDay = false;
-		moon.transform.position = moonHigh;
-		//background.SetActive(true); 
-		background.GetComponent<Animator>().Play("MicroMixBackgroundSky", 0, .5f);//(Maybe we can just use a reset)
-		background.GetComponent<Animator>().speed = 1;
+		Setup();
 	}
 
 	protected override void _EndMicro(){ //Not necessary to set clocks or background inactive, because we are their parent and will set them inactive regardless
-		//background.SetActive(false);
+		LeanTween.cancel(moon);
 	}
 
 	protected override void _Pause(){
 		LeanTween.pause(moon);
-		//Pause all tweens
-		//Pause all animations
+		background.GetComponent<Animator>().enabled = false;
 	}
 
 	protected override void _Resume(){
 		LeanTween.pause(moon);
+		background.GetComponent<Animator>().enabled = true;
+
 		//Resume all tweens
 		//Resume all animations
 	}
 
 	protected override IEnumerator _Tutorial(){
-		yield return 0;
+		Setup();
+		MicroMixFinger finger = MicroMixManager.Instance.finger;
+		TimeItem2 time = GetComponentInChildren<TimeItem2>();
+		Vector3 offset = Vector3.up * .5f;
+		finger.transform.position = time.transform.position + offset; //Set up the finger and the sky...
+
+		while(angle > 255){ //Position ourselves in the first clock
+			yield return 0;
+		}
+
+		paused = true;
+		finger.gameObject.SetActive(true);
+		yield return WaitSecondsPause(.3f); //Position finger on button and wait
+		time.ShrinkDown();
+		yield return finger.MoveTo(time.transform.position + offset, time.transform.position, delay: 0f, time: .15f); //Now press it
+		yield return finger.MoveTo(time.transform.position, time.transform.position + offset, delay: 0f, time: .15f);
+		paused = false;
+		finger.gameObject.SetActive(false);
+		time.clock1.SetActive(false); //Remove the clock and us
+
+		while(angle > 106){ //Position ourselves in the first clock
+			yield return 0;
+		}
+
+		paused = true;
+		finger.gameObject.SetActive(true);
+		yield return WaitSecondsPause(.3f); //Give a visual cue we will press
+		time.ShrinkDown();
+		yield return finger.MoveTo(time.transform.position + offset, time.transform.position, delay: 0f, time: .15f); //Now press it again
+		yield return finger.MoveTo(time.transform.position, time.transform.position + offset, delay: 0f, time: .15f);
+		paused = false;
+		finger.gameObject.SetActive(false); // Remove the clocks etc.
+		time.clock2.SetActive(false);
+
+		yield return WaitSecondsPause(.4f); //Let the sun go beyond horizon
 	}
 
 	private void NightToDay(){
 		//LeanTween.move(moon, moonLow, transitionLength).setEase(LeanTweenType.easeInOutQuad);
-		LeanTween.alpha(moon,0,transitionLength).setEase(LeanTweenType.easeOutQuint);
+		LeanTween.alpha(moon, 0, transitionLength).setEase(LeanTweenType.easeOutQuint);
 	}
 
 	private void DayToNight(){
 		//LeanTween.move(moon, moonHigh, transitionLength).setEase(LeanTweenType.easeInOutQuad);
-		LeanTween.alpha(moon,1,transitionLength).setEase(LeanTweenType.easeInQuint);
+		LeanTween.alpha(moon, 1, transitionLength).setEase(LeanTweenType.easeInQuint);
+	}
+
+	private void Setup(){
+		moon.transform.position = moonHigh;
+		moon.GetComponent<SpriteRenderer>().color = Color.white;
+		background.GetComponent<Animator>().Play("MicroMixBackgroundSky", 0, .54f);
+		background.GetComponent<Animator>().speed = 3 / 4f;
+		angle = 345;
 	}
 }

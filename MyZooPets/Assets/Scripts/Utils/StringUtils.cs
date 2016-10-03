62b2460ff0eefcf798955a0a430054c1cb1ed4e6
aliken9 <aliken9@gmail.com>
2016-10-03 14:58:40 -0400

    Removed color encoding on inhaler button text

--- MyZooPets/Assets/Scripts/Utils/StringUtils.cs
+++ MyZooPets/Assets/Scripts/Utils/StringUtils.cs
@@ -4,17 +4,14 @@ using System.Collections;
 using System.Reflection;
 using System.Collections.Generic;
 
-
 //---------------------------------------------------
 // StringUtils
 // Utility functions for formatting/localizing strings.
 // Jason: A better way to do this is by using System.String.Format ....too late to change now
 //---------------------------------------------------
-
 public class StringUtils {
-
-	public static string FormatStringPossession(string oldString){
-		if(string.IsNullOrEmpty(oldString)){
+	public static string FormatStringPossession(string oldString) {
+		if(string.IsNullOrEmpty(oldString)) {
 			Debug.LogWarning("Formatting an empty string");
 			return "";
 		}
@@ -22,75 +19,70 @@ public class StringUtils {
 		string subString = oldString.Substring(oldString.Length - 1);
 		string newString = "";
 
-		if(subString == "s")
+		if(subString == "s") {
 			newString = oldString + "'";
-		else
+		}
+		else {
 			newString = oldString + "'s";
-
+		}
 		return newString;
 	}
 
-	public static string FormatNumber( int i_nVal ) {
-		string strDelim = Localization.Localize( "NUMBER_DELIMETER" );
+	public static string FormatNumber(int i_nVal) {
+		string strDelim = Localization.Localize("NUMBER_DELIMETER");
 		string strVal = i_nVal.ToString("n0");
-		
 		strVal = strVal.Replace(",", strDelim);
-	
 		return strVal;
 	}
 
-    // <Description StringFormat="">
-    // <Binding Path=""></Binding>
-    // </Description>
-    // working progress......probably need type information for each binding
-    // how to handle plural, and possession cases.
-	public static string FormatString(Type classType, IXMLNode stringBindings){
-	 	List<PropertyInfo> propertyInfo= new List<PropertyInfo>();
-        string retValue = "";
+	/// <summary>
+	/// Probably need type information for each binding how to handle plural, and possession cases.
+	/// </summary>
+	public static string FormatString(Type classType, IXMLNode stringBindings) {
+		List<PropertyInfo> propertyInfo = new List<PropertyInfo>();
 
-        Hashtable hashAttr = XMLUtils.GetAttributes(stringBindings);
-        string stringFormat = "";
+		Hashtable hashAttr = XMLUtils.GetAttributes(stringBindings);
+		string stringFormat = "";
 
-        if(hashAttr.ContainsKey("StringFormat"))
-        	stringFormat = (string) hashAttr["StringFormat"];
-        else
-        	Debug.LogError("string to be formatted can't be found for class " + classType);
+		if(hashAttr.ContainsKey("StringFormat")) {
+			stringFormat = (string)hashAttr["StringFormat"];
+		}
+		else {
+			Debug.LogError("string to be formatted can't be found for class " + classType);
+		}
 
-        //Localize the string	
-        string baseString = Localization.Localize(stringFormat);
+		//Localize the string	
+		string baseString = Localization.Localize(stringFormat);
 
-        //Get all the bindings
-        List<IXMLNode> childrenList = XMLUtils.GetChildrenList(stringBindings);
+		//Get all the bindings
+		List<IXMLNode> childrenList = XMLUtils.GetChildrenList(stringBindings);
 
-        foreach(IXMLNode node in childrenList){
-            Hashtable bindingAttr = XMLUtils.GetAttributes(node);
-            string binding = (string) bindingAttr["Path"];
+		foreach(IXMLNode node in childrenList) {
+			Hashtable bindingAttr = XMLUtils.GetAttributes(node);
+			string binding = (string)bindingAttr["Path"];
+			propertyInfo.Add(classType.GetProperty(binding));
+		}
 
-            propertyInfo.Add(classType.GetProperty(binding));
-        }
-
-        //format the base string with the format items
-        retValue = String.Format(baseString, propertyInfo);
-
-        return retValue;	
+		//format the base string with the format items
+		return string.Format(baseString, propertyInfo);
 	}
 
-	public static Vector3 ParseVector3(string vectorString){
+	public static Vector3 ParseVector3(string vectorString) {
 		Vector3 vector = new Vector3(0, 0, 0);
-		String[] arrayVector3;
-		
-		try{
+		string[] arrayVector3;
+
+		try {
 			arrayVector3 = vectorString.Split(","[0]);
-			if(arrayVector3.Length == 3){
+			if(arrayVector3.Length == 3) {
 				vector = new Vector3(
-					float.Parse(arrayVector3[0].Trim(new char[]{'(' })), 
-					float.Parse(arrayVector3[1]), 
-					float.Parse(arrayVector3[2].Trim(new char[]{')' })));
+					float.Parse(arrayVector3[0].Trim(new char[] { '(' })),
+					float.Parse(arrayVector3[1]),
+					float.Parse(arrayVector3[2].Trim(new char[] { ')' })));
 			}
 			else
 				Debug.LogError("Illegal vector3 parsing, reverting to 0,0,0");
 		}
-		catch(NullReferenceException e){
+		catch(NullReferenceException e) {
 			Debug.LogError("Vector3 parsing. string cannot be null. error message: " + e.Message);
 		}
 		return vector;
@@ -102,16 +94,16 @@ public class StringUtils {
 	/// </summary>
 	/// <returns>Double digit string representation of the number</returns>
 	/// <param name="number">Number to convert</param>
-	public static string FormatIntToDoubleDigitString(int number){
-		if(number > 99 || number < 0){
+	public static string FormatIntToDoubleDigitString(int number) {
+		if(number > 99 || number < 0) {
 			Debug.LogError("Unsupported input for number detected " + number);
 			return "00";
 		}
-		else{
-			if(number > 9){
+		else {
+			if(number > 9) {
 				return number.ToString();
 			}
-			else{
+			else {
 				return "0" + number.ToString();
 			}
 		}
@@ -122,18 +114,15 @@ public class StringUtils {
 	/// </summary>
 	/// <returns>The time left.</returns>
 	/// <param name="timeLeft">Time left</param>
-	public static string FormatTimeLeft(TimeSpan timeLeft){
-		string displayTime = "";
-		if(timeLeft.Hours > 0){
-			displayTime = string.Format("{0}[FFFF33]h[-] {1}[FFFF33]m[-] {2}[FFFF33]s[-]", 
-			                            timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
+	public static string FormatTimeLeft(TimeSpan timeLeft) {
+		if(timeLeft.Hours > 0) {
+			return string.Format("{0}h {1}m {2}s", timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
 		}
-		else if(timeLeft.Minutes > 0){
-			displayTime = string.Format("{0}[FFFF33]m[-] {1}[FFFF33]s[-]", timeLeft.Minutes, timeLeft.Seconds);
+		else if(timeLeft.Minutes > 0) {
+			return string.Format("{0}m {1}s", timeLeft.Minutes, timeLeft.Seconds);
 		}
-		else{
-			displayTime = string.Format("{0}[FFFF33]s[-]", timeLeft.Seconds);
+		else {
+			return string.Format("{0}s", timeLeft.Seconds);
 		}
-		return displayTime;
 	}
 }

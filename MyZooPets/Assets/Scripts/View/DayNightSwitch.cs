e21c44ec7e6dea8c5fcbfa9f185f4bf4fbc2c7ea
aliken9 <aliken9@gmail.com>
2016-10-21 15:01:17 -0400

    Fixing yard assets

--- MyZooPets/Assets/Scripts/View/DayNightSwitch.cs
+++ MyZooPets/Assets/Scripts/View/DayNightSwitch.cs
@@ -1,12 +1,8 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections;
 
 public class DayNightSwitch : MonoBehaviour{
-	
 	public bool isDayTime = true;
-	public Color cameraDayColor = new Color(167f/255f, 219f/255f, 255f/255f, 1);
-	public Color cameraNightColor = new Color(46f/255f, 42f/255f, 92f/255f, 1);
 	public Color materialDayTint = new Color(1, 1, 1, 1);
 	public Color materialNightTint = new Color(46f/255f, 42f/255f, 92f/255f, 1);
 	public Renderer[] rendererList;
@@ -33,7 +29,6 @@ public class DayNightSwitch : MonoBehaviour{
 
 	private void ActivateDay(){
 		isDayTime = true;
-		Camera.main.backgroundColor = cameraDayColor;
 		foreach(Renderer ren in rendererList){
 			ren.material.color = materialDayTint;
 		}
@@ -41,18 +36,8 @@ public class DayNightSwitch : MonoBehaviour{
 	
 	private void ActivateNight(){
 		isDayTime = false;
-		Camera.main.backgroundColor = cameraNightColor;
 		foreach(Renderer ren in rendererList){
 			ren.material.color = materialNightTint;
 		}
 	}
-	
-//	void OnGUI(){
-//		if(GUI.Button(new Rect(100f, 100f, 100f, 100f), "Day")){
-//			ActivateDay();
-//		}
-//		if(GUI.Button(new Rect(200f, 100f, 100f, 100f), "Night")){
-//			ActivateNight();
-//		}
-//	}
 }

9bc7a6bdfd2a45ce4f2a2b00b4257bd7146cca95
aliken9 <aliken9@gmail.com>
2016-11-12 18:00:55 -0500

    Inventory icon, cutscene fixes, zoom fixes

--- MyZooPets/Assets/Scripts/Cutscenes/CutsceneControllerMinipetHatchFireBird.cs
+++ MyZooPets/Assets/Scripts/Cutscenes/CutsceneControllerMinipetHatchFireBird.cs
@@ -1,27 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class CutsceneControllerMinipetHatchFireBird : CutsceneController{
-
+public class CutsceneControllerMinipetHatchFireBird : CutsceneController {
 	public Animation eggAnim;
 	public Animation fireBirdAnim;
 
-	public override void Play(){
+	public override void Play() {
 		fireBirdAnim.gameObject.SetActive(false);
 		eggAnim.gameObject.SetActive(true);
 		eggAnim.Play();
 	}
 
-	public void popBird(){
+	public void popBird() {
 		fireBirdAnim.gameObject.SetActive(true);
 		fireBirdAnim.Play();
 	}
 
-	public void playRollAudio(){
-//		AudioManager.Instance.PlayClip("drumRoll");
+	public void playRollAudio() {
+		//		AudioManager.Instance.PlayClip("drumRoll");
 	}
 
-	public void playHatchAudio(){
+	public void playHatchAudio() {
 		AudioManager.Instance.PlayClip("fanfare3");
 	}
 }

2b1c4b2b0e91af75889c1879c112dfaa309dd20a
aliken9 <aliken9@gmail.com>
2016-12-14 18:12:55 -0500

    Winter accessories WIP

--- MyZooPets/Assets/Scripts/Model/ImmutableData/AccessoryItem.cs
+++ MyZooPets/Assets/Scripts/Model/ImmutableData/AccessoryItem.cs
@@ -4,28 +4,36 @@ using System.Collections;
 /// <summary>
 /// An item that is an accessory
 /// </summary>
-public class AccessoryItem : Item{
+public class AccessoryItem : Item {
 	// The type of decortaion this is
 	private AccessoryTypes eType;
 
 	// decoration type property
-	public AccessoryTypes AccessoryType{
-		get{return eType;}
+	public AccessoryTypes AccessoryType {
+		get { return eType; }
 	}
 
 	private string prefabName;
-
-	public string PrefabName{
-		get{return prefabName;}
+	public string PrefabName {
+		get { return prefabName; }
 	}
 
-	public AccessoryItem(string id, ItemType type, Hashtable hashItemData) : base(id, type, hashItemData){
+	private bool inSeason = true;
+	public bool InSeason {
+		get { return inSeason; }
+	}
+
+	public AccessoryItem(string id, ItemType type, Hashtable hashItemData) : base(id, type, hashItemData) {
 		// Get the type of this decoration
 		string strType = XMLUtils.GetString(hashItemData["AccessoryType"] as IXMLNode);
 		eType = (AccessoryTypes)System.Enum.Parse(typeof(AccessoryTypes), strType);
 
-		if(hashItemData.Contains("PrefabName"))
+		if(hashItemData.Contains("PrefabName")) {
 			prefabName = XMLUtils.GetString(hashItemData["PrefabName"] as IXMLNode);
+		}
 
+		if(hashItemData.Contains("InSeason")) {
+			inSeason = XMLUtils.GetBool(hashItemData["InSeason"] as IXMLNode);
+		}
 	}
 }
---
+++ MyZooPets/Assets/Scripts/SpriteAlternating.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SpriteAlternating : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public SpriteRenderer targetSprite;
+	private bool toggleAux = false;
+
+	void Start() {
+		InvokeRepeating("ToggleSprite", 0, 1f);
+	}
+
+	private void ToggleSprite() {
+		toggleAux = !toggleAux;
+		targetSprite.sprite = toggleAux ? sprite1 : sprite2;
+	}
+}

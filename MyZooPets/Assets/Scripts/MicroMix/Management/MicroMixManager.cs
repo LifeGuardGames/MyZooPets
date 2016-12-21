7c01d609f0e202dc07466aa7116828fc3441c13a
Dylan <dylan@lifeguardgames.com>
2016-12-21 15:55:16 -0500

    fixed sad animation overruling happy animation

--- MyZooPets/Assets/Scripts/MicroMix/Management/MicroMixManager.cs
+++ MyZooPets/Assets/Scripts/MicroMix/Management/MicroMixManager.cs
@@ -278,6 +278,7 @@ public class MicroMixManager : NewMinigameManager<MicroMixManager> {
 		}
 		else {
 			ChangeMicro();
+			petAnim.animator.Play(petAnim.happyIdleAnimations[0], 0, 0);
 			yield return OutTransitionHelper();
 
 			isTransitioning = false;
